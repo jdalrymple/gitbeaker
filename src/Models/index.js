0188312531dfea74332b45ed252b51bf47167f20
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2017-07-06 13:04:03 -0400

    Fixing broken reference

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,8 +1,7 @@
 const Groups = require('./Groups');
 const Projects = require('./Projects');
 const Issues = require('./Issues');
-const Notes = require('./Notes');
 const Users = require('./Users');
 const Labels = require('./Labels');
 
-module.exports = {Groups, Projects, Issues, Notes, Users, Labels}
+module.exports = {Groups, Projects, Issues, Users, Labels}
