ca1f655318d28532e346caa3a6863c3a2da8b49f
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2018-01-18 23:02:22 -0500

    Moving function calls out of the template string for clarity

--- src/Models/ProjectRepositoryFiles.js
+++ src/Models/ProjectRepositoryFiles.js
@@ -4,32 +4,40 @@ import { parse } from '../Utils';
 class ProjectRepositoryFiles extends BaseModel {
   create(projectId, filePath, branch, options = {}) {
     const pId = parse(projectId);
+    const path = parse(filePath);
+    const extendedOptions = Object.assign({ branch }, options);
 
-    return this.post(`projects/${pId}/repository/files/${parse(filePath)}`, Object.assign({ branch }, options));
+    return this.post(`projects/${pId}/repository/files/${path}`, extendedOptions);
   }
 
   edit(projectId, filePath, branch, options = {}) {
     const pId = parse(projectId);
+    const path = parse(filePath);
+    const extendedOptions = Object.assign({ branch }, options);
 
-    return this.put(`projects/${pId}/repository/files/${parse(filePath)}`, Object.assign({ branch }, options));
+    return this.put(`projects/${pId}/repository/files/${path}`, extendedOptions);
   }
 
   remove(projectId, filePath, branch, options = {}) {
     const pId = parse(projectId);
+    const path = parse(filePath);
+    const extendedOptions = Object.assign({ branch }, options);
 
-    return this.delete(`projects/${pId}/repository/files/${parse(filePath)}`, Object.assign({ branch }, options));
+    return this.delete(`projects/${pId}/repository/files/${path}`, extendedOptions);
   }
 
   show(projectId, filePath, ref) {
     const pId = parse(projectId);
+    const path = parse(filePath);
 
-    return this.get(`projects/${pId}/repository/files/${parse(filePath)}`, { ref });
+    return this.get(`projects/${pId}/repository/files/${path}`, { ref });
   }
 
   showRaw(projectId, filePath, ref) {
     const pId = parse(projectId);
+    const path = parse(filePath);
 
-    return this.get(`projects/${pId}/repository/files/${parse(filePath)}/raw`, { ref });
+    return this.get(`projects/${pId}/repository/files/${path}/raw`, { ref });
   }
 }
 
