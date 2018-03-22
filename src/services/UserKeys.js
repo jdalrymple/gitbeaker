6f8f7f6f9414a04448a926f7c3f6d7bc44a031bb
Jordan Wallet <jordanw@axosoft.com>
2018-03-22 09:32:45 -0700

    Fix current user "keys" routes

--- src/services/UserKeys.js
+++ src/services/UserKeys.js
@@ -2,13 +2,13 @@ import { BaseService, RequestHelper } from '../infrastructure';
 
 class UserKeys extends BaseService {
   all({ userId }) {
-    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'users/keys';
+    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'user/keys';
 
     return RequestHelper.get(this, url);
   }
 
   create(title, key, { userId } = {}) {
-    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'users/keys';
+    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'user/keys';
 
     return RequestHelper.post(this, url, {
       title,
@@ -19,12 +19,12 @@ class UserKeys extends BaseService {
   show(keyId) {
     const kId = encodeURIComponent(keyId);
 
-    return RequestHelper.get(this, `users/keys/${kId}`);
+    return RequestHelper.get(this, `user/keys/${kId}`);
   }
 
   remove(keyId, { userId } = {}) {
     const kId = encodeURIComponent(keyId);
-    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'users/keys';
+    const url = userId ? `users/${encodeURIComponent(userId)}/keys` : 'user/keys';
 
     return RequestHelper.delete(this, `${url}/${kId}`);
   }
