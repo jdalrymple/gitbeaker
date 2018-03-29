34e3779e7c24fa80f475d3c24080c12cecea1d59
Justin <justin.s.dalrymple@gmail.com>
2018-03-29 13:40:34 -0400

    Adding extra endpoints to commits

--- src/services/Commits.js
+++ src/services/Commits.js
@@ -7,10 +7,27 @@ class Commits extends BaseService {
     return RequestHelper.get(this, `projects/${pId}/repository/commits`, options);
   }
 
+  cherryPick(projectId, sha, branch) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.post(this, `projects/${pId}/repository/commits/${sha}/cherry_pick`, { branch });
+  }
+
   comments(projectId, sha) {
     const pId = encodeURIComponent(projectId);
 
     return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}/comments`);
+  }
+
+  create(projectId, branch, message, actions = [], options) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.post(this, `projects/${pId}/repository/commits`, {
+      branch,
+      commitMessage: message,
+      actions,
+      ...options,
+    });
   }
 
   createComment(projectId, sha, note, options) {
@@ -28,13 +45,25 @@ class Commits extends BaseService {
     return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}/diff`);
   }
 
+  editStatus(projectId, sha, options) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}/statuses`, options);
+  }
+
+  references(projectId, sha) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}/refs`);
+  }
+
   show(projectId, sha) {
     const pId = encodeURIComponent(projectId);
 
     return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}`);
   }
 
-  statuses(projectId, sha, options) {
+  status(projectId, sha, options) {
     const pId = encodeURIComponent(projectId);
 
     return RequestHelper.get(this, `projects/${pId}/repository/commits/${sha}/statuses`, options);
