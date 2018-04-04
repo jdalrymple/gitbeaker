1732a2c4a6f747acb96566f4d5e567026a7ca2a1
Jordan Wallet <jordanw@axosoft.com>
2018-04-04 12:36:41 -0700

    Make XmlHttpRequest respect resolveWithFullResponse

--- src/infrastructure/XMLHttpRequester.js
+++ src/infrastructure/XMLHttpRequester.js
@@ -1,13 +1,25 @@
 import Promisify from 'util.promisify';
 import XHR from 'xhr';
 
-const XMLHttpRequester = Promisify(XHR);
-XMLHttpRequester.del = Promisify(XHR.del);
+function PromisifyWithFullResponseOption(fn) {
+  const promisifiedFn = Promisify(fn);
+
+  return async (params) => {
+    const response = await promisifiedFn(params);
+
+    return params.resolveWithFullResponse
+      ? response
+      : response.body;
+  };
+}
+
+const XMLHttpRequester = PromisifyWithFullResponseOption(XHR);
+XMLHttpRequester.del = PromisifyWithFullResponseOption(XHR.del);
 XMLHttpRequester.delete = XMLHttpRequester.del;
-XMLHttpRequester.get = Promisify(XHR.get);
-XMLHttpRequester.head = Promisify(XHR.head);
-XMLHttpRequester.patch = Promisify(XHR.patch);
-XMLHttpRequester.post = Promisify(XHR.post);
-XMLHttpRequester.put = Promisify(XHR.put);
+XMLHttpRequester.get = PromisifyWithFullResponseOption(XHR.get);
+XMLHttpRequester.head = PromisifyWithFullResponseOption(XHR.head);
+XMLHttpRequester.patch = PromisifyWithFullResponseOption(XHR.patch);
+XMLHttpRequester.post = PromisifyWithFullResponseOption(XHR.post);
+XMLHttpRequester.put = PromisifyWithFullResponseOption(XHR.put);
 
 export default XMLHttpRequester;
