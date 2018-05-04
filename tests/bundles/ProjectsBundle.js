0ae9065bf9e0671b3b58d62357690d930523639d
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2018-05-04 12:54:58 -0400

    Moving over the template projects test

---
+++ tests/bundles/ProjectsBundle.js
@@ -0,0 +1,57 @@
+import { ProjectsBundle } from '../../src';
+import Services from '../../src/services';
+
+test('All the correct service keys are included in the projects bundle', async () => {
+  const bundle = new ProjectsBundle();
+  const services = [
+    'Branches',
+    'Commits',
+    'DeployKeys',
+    'Deployments',
+    'Environments',
+    'Issues',
+    'IssueAwardEmojis',
+    'IssueNotes',
+    'IssueDiscussions',
+    'Jobs',
+    'Labels',
+    'MergeRequests',
+    'MergeRequestAwardEmojis',
+    'MergeRequestNotes',
+    'Pipelines',
+    'PipelineSchedules',
+    'PipelineScheduleVariables',
+    'Projects',
+    'ProjectAccessRequests',
+    'ProjectBadges',
+    'ProjectCustomAttributes',
+    'ProjectImportExport',
+    'ProjectIssueBoards',
+    'ProjectHooks',
+    'ProjectMembers',
+    'ProjectMilestones',
+    'ProjectSnippet',
+    'ProjectSnippetNotes',
+    'ProjectSnippetDiscussions',
+    'ProjectSnippetAwardEmojis',
+    'ProtectedBranches',
+    'ProjectVariables',
+    'Repositories',
+    'RepositoryFiles',
+    'Runners',
+    'Services',
+    'Tags',
+    'Triggers',
+  ]
+
+  expect(bundle).toHaveProperty(services);
+});
+
+test('All the correct service instances are included in the projects bundle', async () => {
+  const bundle = new ProjectsBundle();
+
+  Object.keys(bundle).forEach(key => {
+    expect(bundle[key]).instanceOf(Services[key]);
+  });
+});
+
--- tests/infrastructure/BaseService.js
+++ tests/infrastructure/BaseService.js
@@ -1,38 +1,40 @@
 import { BaseService } from '../../src/infrastructure';
 
-test('If a token or oauthToken is not passed, throw an error', async () => {
-  expect(() => {
-    const service = new BaseService();
-  }).toThrowError('`token` (private-token) or `oauth_token` is mandatory');
-});
+describe('Creation of BaseService instance', () => {
+  test('If a token or oauthToken is not passed, throw an error', async () => {
+    expect(() => {
+      const service = new BaseService();
+    }).toThrowError('`token` (private-token) or `oauth_token` is mandatory');
+  });
 
-test('Url defaults to https://gitlab.com/api/v4', async () => {
-  const service = new BaseService({ token: 'test' });
+  test('Url defaults to https://gitlab.com/api/v4', async () => {
+    const service = new BaseService({ token: 'test' });
 
-  expect(service.url).toBe('https://gitlab.com/api/v4');
-});
+    expect(service.url).toBe('https://gitlab.com/api/v4');
+  });
 
-test('Use the Oauth Token when a user supplies both a Private Token and a Oauth Token', async () => {
-  const service = new BaseService({ token: 'test', oauthToken: '1234' });
+  test('Use the Oauth Token when a user supplies both a Private Token and a Oauth Token', async () => {
+    const service = new BaseService({ token: 'test', oauthToken: '1234' });
 
-  expect(service.headers['private-token']).toBeUndefined();
-  expect(service.headers.authorization).toBe('Bearer 1234');
-});
+    expect(service.headers['private-token']).toBeUndefined();
+    expect(service.headers.authorization).toBe('Bearer 1234');
+  });
 
-test('Custom url still appends api and version number to url', async () => {
-  const service = new BaseService({ url: 'https://testing.com', token: 'test' });
+  test('Custom url still appends api and version number to url', async () => {
+    const service = new BaseService({ url: 'https://testing.com', token: 'test' });
 
-  expect(service.url).toBe('https://testing.com/api/v4');
-});
+    expect(service.url).toBe('https://testing.com/api/v4');
+  });
 
-test('Oauth token adds to authorization header as a bearer token', async () => {
-  const service = new BaseService({ url: 'https://testing.com', oauthToken: '1234' });
+  test('Oauth token adds to authorization header as a bearer token', async () => {
+    const service = new BaseService({ url: 'https://testing.com', oauthToken: '1234' });
 
-  expect(service.headers.authorization).toBe('Bearer 1234');
-});
+    expect(service.headers.authorization).toBe('Bearer 1234');
+  });
 
-test('Private token adds to private-token header', async () => {
-  const service = new BaseService({ url: 'https://testing.com', token: '1234' });
+  test('Private token adds to private-token header', async () => {
+    const service = new BaseService({ url: 'https://testing.com', token: '1234' });
 
-  expect(service.headers['private-token']).toBe('1234');
-});
+    expect(service.headers['private-token']).toBe('1234');
+  });
+}
