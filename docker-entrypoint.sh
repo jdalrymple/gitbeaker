19afc30fa62c0cf22371d178263828f3314d8fea
Justin <justin.s.dalrymple@gmail.com>
2018-05-13 13:17:55 -0400

    Still working on the entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+## Set GITLAB_URL ENV
+gitlab-rails console production
+
+## Reset default password for root user
+u = User.first
+u.password_automatically_set = false
+u.password = 'password'
+u.password_confirmation = 'password'
+u.save!
+
+## Create access token
+t = PersonalAccessToken.new({ user: u, name: 'node-gitlab', scopes: ['api']})
+t.save!
+
+## Store access token in an ENV
+ENV['PERSONAL_ACCESS_TOKEN'] = t.token
+
+## Exist rails console
+exit
+
+## Exec any other entry point logic
+exec "$@"
