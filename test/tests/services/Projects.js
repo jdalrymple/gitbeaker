95b8081d0c2ed4ab3f1e22c235ebcd3e002dfacf
Justin <justin.s.dalrymple@gmail.com>
2018-05-16 23:35:05 -0400

    Updating travis

--- test/tests/services/Projects.js
+++ test/tests/services/Projects.js
@@ -1,8 +1,13 @@
 import { Projects } from '../../src';
 
+const credentials = {
+	url: process.env.GITLAB_URL,
+	token: process.env.PERSONAL_ACCESS_TOKEN
+}
+
 describe('Projects.all', () => {
   it('should return empty array', async () => {
-    const service = new Projects({ url: process.env.GITLAB_URL, token: process.env.API_TOKEN });
+    const service = new Projects(credentials);
     const projects = await service.all();
 
     expect(projects).toEqual([]);
