4642b123eeda80d5d82fadcdb8ceb0bc53c61b5c
Justin <justin.s.dalrymple@gmail.com>
2018-05-17 18:34:58 -0400

    Updating projects test

--- test/tests/services/Projects.js
+++ test/tests/services/Projects.js
@@ -1,13 +1,8 @@
 import { Projects } from '../../../src';
 
-const credentials = {
-	url: process.env.GITLAB_URL,
-	token: process.env.PERSONAL_ACCESS_TOKEN
-}
-
 describe('Projects.all', () => {
   it('should return empty array', async () => {
-    const service = new Projects(credentials);
+    const service = new Projects({ url: process.env.GITLAB_URL, token: process.env.PERSONAL_ACCESS_TOKEN });
     const projects = await service.all();
 
     expect(projects).toEqual([]);
