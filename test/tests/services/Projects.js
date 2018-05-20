08eb791566aba7fc5f30e805ff02df63d74a88b9
Justin <justin.s.dalrymple@gmail.com>
2018-05-20 15:28:09 -0400

    Debugging tests

--- test/tests/services/Projects.js
+++ test/tests/services/Projects.js
@@ -2,7 +2,10 @@ import { Projects } from '../../../src';
 
 describe('Projects.all', () => {
   it('should return empty array', async () => {
-    const service = new Projects({ url: process.env.GITLAB_URL, token: process.env.PERSONAL_ACCESS_TOKEN });
+  	const c = { url: process.env.GITLAB_URL, token: process.env.PERSONAL_ACCESS_TOKEN };
+
+  	console.log(c)
+    const service = new Projects(c);
     const projects = await service.all();
 
     expect(projects).toEqual([]);
