db53a105725f40255ec5cd037b714ff5977b2757
Justin <justin.s.dalrymple@gmail.com>
2018-05-22 18:51:56 -0400

    See if  it affects all envs

--- test/tests/services/Projects.js
+++ test/tests/services/Projects.js
@@ -4,7 +4,7 @@ describe('Projects.all', () => {
   it('should return empty array', async () => {
   	const c = { url: process.env.GITLAB_URL, token: process.env.PERSONAL_ACCESS_TOKEN };
 
-  	console.log(c)
+  	console.log(process.env.TESTS)
     const service = new Projects(c);
     const projects = await service.all();
 
