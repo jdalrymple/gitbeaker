b4aa7ac2e9bc3cd227054c710678886004ee76ed
Pavel Birukov <birukov.pavel@gmail.com>
2018-05-28 14:59:48 +0300

    Rename test/ files to ts. Introduce ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,16 @@ module.exports = {
   coverageDirectory: './coverage/',
   collectCoverage: true,
   setupTestFrameworkScriptFile: 'jest-extended',
-  testRegex: '(/__tests__/.*|/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
   reporters: ['jest-tap-reporter'],
+  testRegex: '(/__tests__/.*|/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json"
+  ]
 };
