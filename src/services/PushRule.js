74464513c7cb57f4ee001128a02a443931e23a92
Joseph Petersen <josephp90@gmail.com>
2018-08-10 15:50:14 +0200

    feat: add push rule service

---
+++ src/services/PushRule.js
@@ -0,0 +1,37 @@
+import { BaseService, RequestHelper } from '../infrastructure';
+
+class PushRule extends BaseService {
+
+  create(projectId, options) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.post(this, `projects/${pId}/push_rule`, options);
+  }
+
+  async edit(projectId, { upsert, ...options } = {}) {
+    const pId = encodeURIComponent(projectId);
+
+    if (upsert) {
+      const pushRule = await this.show(projectId);
+
+      if (!pushRule) return this.create(projectId, options)
+    }
+
+    return RequestHelper.put(this, `projects/${pId}/push_rule`, args);
+  }
+
+  remove(projectId) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.delete(this, `projects/${pId}/push_rule`);
+  }
+
+  show(projectId) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.get(this, `projects/${pId}/push_rule`);
+  }
+
+}
+
+export default PushRule;
