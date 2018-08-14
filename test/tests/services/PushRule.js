ee0be2d79d8e2790bff7082b72998d7ec5b881ad
Pavel Birukov <birukov.pavel@gmail.com>
2018-08-14 20:18:09 +0300

    Make CI green again (#150)
    * Disable cache on travis
    
    * Fix linter failures
    
    * Disable PushRule test (EE-only feature)

--- test/tests/services/PushRule.js
+++ test/tests/services/PushRule.js
@@ -1,15 +1,16 @@
 import { PushRule } from '../../../src';
 
 describe('PushRule', () => {
-  it('should create or edit push rule on upsert', async () => {
+  // the feature is not available for CE users https://gitlab.com/gitlab-org/gitlab-ee/issues/3825
+  xit('should create or edit push rule on upsert', async () => {
     const service = new PushRule({
       url: process.env.GITLAB_URL,
       token: process.env.PERSONAL_ACCESS_TOKEN,
     });
     const pushRules = {
       upsert: true,
-      member_check: true
-    }
+      member_check: true,
+    };
     const result = await service.edit(1, pushRules);
 
     expect(result).toBeInstanceOf(Object);
