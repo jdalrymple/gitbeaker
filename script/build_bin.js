eb501e9ecf225dd600b816cf5538da4d17e55321
caojun <78612846@qq.com>
2018-08-15 11:26:18 +0800

    Refactor build_bin by using Promise.all

--- script/build_bin.js
+++ script/build_bin.js
@@ -1,10 +1,15 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+
 const fs = require('fs');
 const fse = require('fs-extra');
+const Promise = require('bluebird');
 
 const kebabCase = require('lodash.kebabcase');
 
 const ES6_COMMAND = 'gitlab';
 const ES5_COMMAND = 'gitlabe5';
+
+const writeFile = Promise.promisify(fs.writeFile);
 
 function buildBinDirectory(command, path) {
   const nameArray = fs.readdirSync('./src/services').map((file) => {
@@ -22,12 +27,12 @@ function buildBinDirectory(command, path) {
   });
   fse.writeJsonSync('./package.json', packageInfo, { spaces: 2 });
 
-  nameArray.forEach((name) => {
-    fs.writeFileSync(`./bin/${name}`,
-      `#!/usr/bin/env node \n
-require('../dist/${path}/cli/index').default('${name}');`);
-  });
+  return Promise.all(nameArray.map(name => writeFile(`./bin/${name}`,
+    `#!/usr/bin/env node \n require('../dist/${path}/cli/index').default('${name}');`)));
 }
 
-buildBinDirectory(ES6_COMMAND, 'latest');
-buildBinDirectory(ES5_COMMAND, 'es5');
+Promise.all(buildBinDirectory(ES6_COMMAND, 'latest'), buildBinDirectory(ES5_COMMAND, 'es5')).then(() => {
+  console.log('Build bin finished');
+}).catch((error) => {
+  console.error(error.message);
+});
