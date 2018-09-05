5523ca46b5dc06066901961888e46f82679ec30b
Pavel Birukov <birukov.pavel@gmail.com>
2018-09-05 19:45:13 +0300

    Annotate Bundler

--- src/infrastructure/Bundler.ts
+++ src/infrastructure/Bundler.ts
@@ -1,13 +1,21 @@
-function Bundler(services = {}) {
-  const combined = { ...services };
+import { BaseService } from '.';
+import { BaseModelContructorOptions } from './BaseService';
 
-  return class Bundle {
-    constructor(options = {}) {
+function Bundler<T extends { [K: string]: typeof BaseService }>(
+  services: T,
+): new (...args: any[]) => { [K in keyof T]: InstanceType<T[K]> } {
+  const combined = Object.assign({}, services);
+  interface BundleClass {
+    [K: string]: BaseService;
+  }
+  return class Bundle implements BundleClass {
+    [K: string]: any;
+    constructor(options: BaseModelContructorOptions = {}) {
       Object.keys(combined).forEach((serviceName) => {
         this[serviceName] = new combined[serviceName](options);
       });
     }
-  };
+  } as any;
 }
 
 export default Bundler;
