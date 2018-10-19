05de064410fd16c9e29017f677888cfb9319f426
Justin <justin.s.dalrymple@gmail.com>
2018-10-18 21:57:57 -0400

    chore: Updating configuration and packages

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import ts from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import { terser } from "rollup-plugin-terser";
+import typescript from 'typescript';
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/bundle.js',
+      name: 'node-gitlab',
+      format: 'umd',
+      exports: 'named'
+    },
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+      json(),
+      resolve({ browser: true }), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      ts({ typescript }),
+      terser(),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    external: [...Object.keys(pkg.dependencies)],
+    plugins: [
+      ts({ typescript }),
+      terser(),
+    ],
+  },
+];
