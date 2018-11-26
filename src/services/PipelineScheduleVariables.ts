28b28cfb21b0c3a9b82f1269abdea7e71e17265e
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2018-11-26 14:36:37 -0500

    fix: Project variables urls were being generated incorrectly

--- src/services/PipelineScheduleVariables.ts
+++ src/services/PipelineScheduleVariables.ts
@@ -1,9 +1,52 @@
-import { ResourceVariables } from '../templates';
+import { BaseService, RequestHelper } from '../infrastructure';
 import { BaseModelContructorOptions } from '../infrastructure/BaseService';
+import { RequestOptions } from '../infrastructure/RequestHelper';
 
-class PipelineScheduleVariables extends ResourceVariables {
-  constructor(options: BaseModelContructorOptions) {
-    super('projects', 'pipelines', options);
+class PipelineScheduleVariables extends BaseService {
+  all(projectId: ProjectId, pipelineScheduleId: PipelineScheduleId) {
+    const [pId, psId] = [projectId, pipelineScheduleId].map(encodeURIComponent);
+
+    return RequestHelper.get(this, `projects/${pId}/pipeline_schedules/${psId}/variables`);
+  }
+
+  create(projectId: ProjectId, pipelineScheduleId: PipelineScheduleId, options: RequestOptions) {
+    const [pId, psId] = [projectId, pipelineScheduleId].map(encodeURIComponent);
+
+    return RequestHelper.post(
+      this,
+      `projects/${pId}/pipeline_schedules/${psId}/variables`,
+      options,
+    );
+  }
+
+  edit(
+    projectId: ProjectId,
+    pipelineScheduleId: PipelineScheduleId,
+    keyId: string,
+    options: RequestOptions,
+  ) {
+    const [pId, psId, kId] = [projectId, pipelineScheduleId, keyId].map(encodeURIComponent);
+
+    return RequestHelper.put(
+      this,
+      `projects/${pId}/pipeline_schedules/${psId}/variables/${kId}`,
+      options,
+    );
+  }
+
+  show(projectId: ProjectId, pipelineScheduleId: PipelineScheduleId, keyId: string) {
+    const [pId, psId, kId] = [projectId, pipelineScheduleId, keyId].map(encodeURIComponent);
+
+    return RequestHelper.get(this, `projects/${pId}/pipeline_schedules/${psId}/variables/${kId}`);
+  }
+
+  remove(projectId: ProjectId, pipelineScheduleId: PipelineScheduleId, keyId: string) {
+    const [pId, psId, kId] = [projectId, pipelineScheduleId, keyId].map(encodeURIComponent);
+
+    return RequestHelper.delete(
+      this,
+      `projects/${pId}/pipeline_schedules/${psId}/variables/${kId}`,
+    );
   }
 }
 
--- src/templates/ResourceVariables.ts
+++ src/templates/ResourceVariables.ts
@@ -2,80 +2,43 @@ import { BaseService, RequestHelper } from '../infrastructure';
 import { BaseModelContructorOptions } from '../infrastructure/BaseService';
 import { RequestOptions } from '../infrastructure/RequestHelper';
 
-function url(
-  resourceType: ResourceType,
-  resourceId: ResourceId,
-  resource2Type: Resource2Type,
-  resource2Id: Resource2Id) {
-  const [rId, r2Id] = [resourceId, resource2Id].map(encodeURIComponent);
-
-  let output = `${resourceType}/${rId}/`;
-
-  if (resource2Id) {
-    output += `${resource2Type}/${r2Id}/`;
-  }
-
-  output += 'variables';
-
-  return output;
-}
-
 class ResourceVariables extends BaseService {
   protected resourceType: ResourceType;
-  protected resource2Type: Resource2Type;
 
-  constructor(
-    resourceType: ResourceType,
-    resource2Type: Resource2Type,
-    baseParams: BaseModelContructorOptions,
-  ) {
+  constructor(resourceType: ResourceType, baseParams: BaseModelContructorOptions) {
     super(baseParams);
 
     this.resourceType = resourceType;
-    this.resource2Type = resource2Type;
   }
 
-  all(resourceId: ResourceType, resource2Id: Resource2Id) {
-    return RequestHelper.get(
-      this,
-      url(this.resourceType, resourceId, this.resource2Type, resource2Id),
-    );
+  all(resourceId: ResourceId) {
+    const rId = encodeURIComponent(resourceId);
+
+    return RequestHelper.get(this, `${this.resourceType}/${rId}/variables`);
   }
 
-  create(resourceId: ResourceType, resource2Id: Resource2Id, options: RequestOptions) {
-    return RequestHelper.post(
-      this,
-      url(this.resourceType, resourceId, this.resource2Type, resource2Id),
-      options,
-    );
+  create(resourceId: ResourceId, options: RequestOptions) {
+    const rId = encodeURIComponent(resourceId);
+
+    return RequestHelper.post(this, `${this.resourceType}/${rId}/variables`, options);
   }
 
-  edit(resourceId: ResourceType, resource2Id: Resource2Id, keyId: string, options: RequestOptions) {
-    const kId = encodeURIComponent(keyId);
+  edit(resourceId: ResourceId, keyId: string, options: RequestOptions) {
+    const [kId, rId] = [resourceId, keyId].map(encodeURIComponent);
 
-    return RequestHelper.put(
-      this,
-      `${url(this.resourceType, resourceId, this.resource2Type, resource2Id)}/${kId}`,
-      options,
-    );
+    return RequestHelper.put(this, `${this.resourceType}/${rId}/variables/${kId}`, options);
   }
 
-  show(resourceId: ResourceType, resource2Id: Resource2Id, keyId: string) {
-    const kId = encodeURIComponent(keyId);
+  show(resourceId: ResourceId, keyId: string) {
+    const [kId, rId] = [resourceId, keyId].map(encodeURIComponent);
 
-    return RequestHelper.get(
-      this,
-      `${url(this.resourceType, resourceId, this.resource2Type, resource2Id)}/${kId}`,
-    );
+    return RequestHelper.get(this, `${this.resourceType}/${rId}/variables/${kId}`);
   }
 
-  remove(resourceId: ResourceType, resource2Id: Resource2Id, keyId: string) {
-    const kId = encodeURIComponent(keyId);
+  remove(resourceId: ResourceId, keyId: string) {
+    const [kId, rId] = [resourceId, keyId].map(encodeURIComponent);
 
-    return RequestHelper.delete(
-      this,
-      `${url(this.resourceType, resourceId, this.resource2Type, resource2Id)}/${kId}`,
-    );
+    return RequestHelper.delete(this, `${this.resourceType}/${rId}/variables/${kId}`);
   }
 }
 
