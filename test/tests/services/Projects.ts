8eb970976d41876a0c7d559795f5a0a375ae8545
Justin <justin.s.dalrymple@gmail.com>
2018-11-28 18:00:30 -0500

    Changed tests to calls instead of instance

--- test/tests/services/Projects.ts
+++ test/tests/services/Projects.ts
@@ -10,7 +10,6 @@ beforeEach(() => {
   service = new Projects({
     host: process.env.GITLAB_URL,
     token: process.env.PERSONAL_ACCESS_TOKEN,
-    requestHandler: new RequestHelper(),
   });
 });
 
@@ -18,8 +17,8 @@ describe('Projects.all', () => {
   it('should request GET /projects', async () => {
     await service.all();
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].get)
-      .toHaveBeenCalledWith(service, 'projects', undefined);
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].get)
+      .toHaveBeenCalledWith(service, 'projects');
   });
 });
 
@@ -27,14 +26,14 @@ describe('Projects.archive', () => {
   it('should request POST /projects/:id/archive', async () => {
     await service.archive(12);
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].post)
-      .toHaveBeenCalledWith(service, 'projects/12/archive', undefined);
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].post)
+      .toHaveBeenCalledWith(service, 'projects/12/archive');
   });
 
   it('should request POST /projects/:id/archive with sudo', async () => {
     await service.archive(12, { sudo: 2 });
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].post)
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].post)
       .toHaveBeenCalledWith(service, 'projects/12/archive', { sudo: 2 });
   });
 });
@@ -43,14 +42,14 @@ describe('Projects.create', () => {
   it('should request POST /projects when userId undefined', async () => {
     await service.create({ title: 'test proj' });
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].post)
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].post)
       .toHaveBeenCalledWith(service, 'projects', { title: 'test proj' });
   });
 
   it('should request POST /projects/user/:id when userId defined', async () => {
     await service.create({ userId: 2, title: 'test proj' });
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].post)
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].post)
       .toHaveBeenCalledWith(service, 'projects/user/2', { title: 'test proj' });
   });
 });
@@ -59,7 +58,7 @@ describe('Projects.edit', () => {
   it('should request PUT /projects', async () => {
     await service.edit(12, { title: 'test proj 2' });
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].put)
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].put)
       .toHaveBeenCalledWith(service, 'projects/12', { title: 'test proj 2' });
   });
 });
@@ -68,8 +67,8 @@ describe('Projects.events', () => {
   it('should request GET /projects/:id/events', async () => {
     await service.events(12);
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].get)
-      .toHaveBeenCalledWith(service, 'projects/12/events', undefined);
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].get)
+      .toHaveBeenCalledWith(service, 'projects/12/events');
   });
 });
 
@@ -77,7 +76,7 @@ describe('Projects.fork', () => {
   it('should request POST /projects/:id/fork', async () => {
     await service.fork(12);
 
-    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.instances[0].post)
-      .toHaveBeenCalledWith(service, 'projects/12/fork', undefined);
+    expect((<jest.Mock<RequestHelper>>RequestHelper).mock.calls[0].post)
+      .toHaveBeenCalledWith(service, 'projects/12/fork');
   });
 });
