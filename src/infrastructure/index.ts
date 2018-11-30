1197069ee36150a4905de0c38e0396f5a131bb8b
Justin <justin.s.dalrymple@gmail.com>
2018-11-29 22:01:10 -0500

    Fixing export

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,5 +1,5 @@
 import * as RequestHelper from './RequestHelper';
 
-export RequestHelper
+export { RequestHelper }
 export { default as BaseService } from './BaseService';
 export { bundler } from './Utils';
