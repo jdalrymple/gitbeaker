b607020e314b4c9a510c23013da11f7a0e9c0fbd
Justin <justin.s.dalrymple@gmail.com>
2019-03-03 13:28:13 -0500

    Tinkering, trying to find the location of the discrepancy

--- src/infrastructure/Utils.ts
+++ src/infrastructure/Utils.ts
@@ -1,10 +1,15 @@
 import { Constructor, Bundle } from '../../types/types';
 
 export function bundler<T extends { [name: string]: Constructor }, P extends keyof T>(services: T){
+	console.log(Object.keys(services).length)
+
 	return (function Bundle(options?: any) {
 		Object.entries(services || {}).forEach(([name, ser]) => {
 			this[name] = new ser(options);
 		});
+
+		console.log(Object.keys(this).length)
+
 	} as any) as Bundle<T, P>;
 }
 
