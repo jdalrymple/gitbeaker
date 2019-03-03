fe8681731cd5d316745d1073815d7096434473a4
Justin <justin.s.dalrymple@gmail.com>
2019-03-03 12:11:12 -0500

    Including import example for commonjs imports

--- test/unit/bundles/Gitlab.ts
+++ test/unit/bundles/Gitlab.ts
@@ -1,9 +1,19 @@
-import { Gitlab } from '../../../src';
+import { Gitlab as GitlabModule } from '../../../src';
 import * as Services from '../../../src/services';
 
+const { Gitlab: GitlabCommon } = require('../../../src');
+
 describe('Instantiating All services', () => {
-  it('should create a valid gitlab service object', async () => {
-  	const bundle = new Gitlab({
+  it('should create a valid gitlab service object using import', async () => {
+  	const bundle = new GitlabModule({
+	    token: 'abcdefg',
+	});
+
+  	expect(Object.keys(bundle)).toEqual(expect.arrayContaining(Object.keys(Services)));
+  });
+
+   it('should create a valid gitlab service object using require', async () => {
+  	const bundle = new GitlabCommon({
 	    token: 'abcdefg',
 	});
 
