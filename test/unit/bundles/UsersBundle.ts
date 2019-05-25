c10690c6097c9c609d4bf008c5240d6b62fdb90b
Justin <justin.s.dalrymple@gmail.com>
2019-05-25 18:09:02 -0400

    test: More jest matcher updates

--- test/unit/bundles/UsersBundle.ts
+++ test/unit/bundles/UsersBundle.ts
@@ -3,15 +3,22 @@ import * as Services from '../../../src/services';
 
 test('All the correct service keys are included in the users bundle', async () => {
   const bundle = new UsersBundle({ token: 'test' });
-  const services = ['Users', 'UserEmails', 'UserCustomAttributes', 'UserImpersonationTokens', 'UserKeys', 'UserGPGKeys'];
+  const services = [
+    'Users',
+    'UserEmails',
+    'UserCustomAttributes',
+    'UserImpersonationTokens',
+    'UserKeys',
+    'UserGPGKeys',
+  ];
 
-  expect(Object.keys(bundle)).toEqual(expect.arrayContaining(services));
+  expect(Object.keys(bundle)).toIncludeAllMembers(services);
 });
 
 test('All the correct service instances are included in the users bundle', async () => {
   const bundle = new UsersBundle({ token: 'test' });
 
-  (Object.keys(bundle) as (keyof typeof bundle)[]).forEach((key) => {
+  (Object.keys(bundle) as (keyof typeof bundle)[]).forEach(key => {
     expect(bundle[key]).toBeInstanceOf(Services[key]);
   });
 });
