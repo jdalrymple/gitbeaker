106e112dbea81c8ac8918e042bdf6552fd4784c5
Justin <justin.s.dalrymple@gmail.com>
2019-05-25 18:08:34 -0400

    test: General updates to various tests to use the proper jest matchers

---
+++ test/unit/services/Projects.ts
@@ -0,0 +1,103 @@
+import { Projects } from '../../../src';
+import { RequestHelper } from '../../../src/infrastructure';
+
+jest.mock('../../../src/infrastructure/RequestHelper');
+jest.mock('../../../src/infrastructure/KyRequester', () => ({
+  // This wont work for all cases, but for the tests currently outlined below, it should be fine
+  get: jest.fn(() => {
+    body: [];
+  }),
+  post: jest.fn(() => {
+    body: {
+    }
+  }),
+  put: jest.fn(() => {
+    body: {
+    }
+  }),
+}));
+
+let service: Projects;
+
+beforeEach(() => {
+  service = new Projects({
+    token: 'abcdefg',
+  });
+});
+
+describe('Instantiating Projects service', () => {
+  it('should create a valid service object', async () => {
+    expect(service).toBeInstanceOf(Projects);
+    expect(service.url).toBeDefined();
+    expect(service.rejectUnauthorized).toBeTruthy();
+    expect(service.headers).toMatchObject({ 'private-token': 'abcdefg' });
+  });
+});
+
+describe('Projects.all', () => {
+  it('should request GET /projects', async () => {
+    await service.all();
+
+    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'projects', undefined);
+  });
+});
+
+describe('Projects.archive', () => {
+  it('should request POST /projects/:id/archive', async () => {
+    await service.archive(12);
+
+    expect(RequestHelper.post).toHaveBeenCalledWith(service, 'projects/12/archive', undefined);
+  });
+
+  it('should request POST /projects/:id/archive with sudo', async () => {
+    await service.archive(12, { sudo: 2 });
+
+    expect(RequestHelper.post).toHaveBeenCalledWith(service, 'projects/12/archive', {
+      sudo: 2,
+    });
+  });
+});
+
+describe('Projects.create', () => {
+  it('should request POST /projects when userId undefined', async () => {
+    await service.create({ title: 'test proj' });
+
+    expect(RequestHelper.post).toHaveBeenCalledWith(service, 'projects', {
+      title: 'test proj',
+    });
+  });
+
+  it('should request POST /projects/user/:id when userId defined', async () => {
+    await service.create({ userId: 2, title: 'test proj' });
+
+    expect(RequestHelper.post).toHaveBeenCalledWith(service, 'projects/user/2', {
+      title: 'test proj',
+    });
+  });
+});
+
+describe('Projects.edit', () => {
+  it('should request PUT /projects', async () => {
+    await service.edit(12, { title: 'test proj 2' });
+
+    expect(RequestHelper.put).toHaveBeenCalledWith(service, 'projects/12', {
+      title: 'test proj 2',
+    });
+  });
+});
+
+describe('Projects.events', () => {
+  it('should request GET /projects/:id/events', async () => {
+    await service.events(12);
+
+    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'projects/12/events', undefined);
+  });
+});
+
+describe('Projects.fork', () => {
+  it('should request POST /projects/:id/fork', async () => {
+    await service.fork(12);
+
+    expect(RequestHelper.post).toHaveBeenCalledWith(service, 'projects/12/fork', undefined);
+  });
+});
