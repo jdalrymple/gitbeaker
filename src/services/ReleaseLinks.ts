d6a2248d369b1b05515f1ddc32971ff483401eba
Justin <justin.s.dalrymple@gmail.com>
2019-06-06 12:46:47 -0400

    feat: Adding support for ReleaseLinks API

---
+++ src/services/ReleaseLinks.ts
@@ -0,0 +1,44 @@
+import { BaseService, RequestHelper } from '../infrastructure';
+
+class ReleaseLinks extends BaseService {
+  all(projectId: ProjectId, tagName: string, options?: PaginatedRequestOptions) {
+    const [pId, tId] = [projectId, tagName].map(encodeURIComponent);
+
+    return RequestHelper.get(this, `projects/${pId}/releases/${tId}/assets/links`, options);
+  }
+
+  create(projectId: ProjectId, tagName: string, name: string, url: string, options?: Sudo) {
+    const [pId, tId] = [projectId, tagName].map(encodeURIComponent);
+
+    return RequestHelper.post(this, `projects/${pId}/releases/${tId}/assets/links`, {
+      name,
+      url,
+      ...options,
+    });
+  }
+
+  edit(
+    projectId: ProjectId,
+    tagName: string,
+    linkId: number,
+    options: Sudo & ({ name: string } | { url: string }),
+  ) {
+    const [pId, tId, lId] = [projectId, tagName, linkId].map(encodeURIComponent);
+
+    return RequestHelper.put(this, `projects/${pId}/releases/${tId}/assets/links/${lId}`, options);
+  }
+
+  remove(projectId: ProjectId, tagName: string, linkId: number, options?: Sudo) {
+    const [pId, tId, lId] = [projectId, tagName, linkId].map(encodeURIComponent);
+
+    return RequestHelper.del(this, `projects/${pId}/releases/${tId}/assets/links/${lId}`, options);
+  }
+
+  show(projectId: ProjectId, tagName: string, linkId: number, options?: Sudo) {
+    const [pId, tId, lId] = [projectId, tagName, linkId].map(encodeURIComponent);
+
+    return RequestHelper.get(this, `projects/${pId}/releases/${tId}/assets/links/${lId}`, options);
+  }
+}
+
+export default ReleaseLinks;
