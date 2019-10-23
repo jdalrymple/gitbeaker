9297638e87103a2302e4fba8947d480b1fa8e000
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2019-10-23 13:38:39 +0200

    test: Updating test scripts to not have fixed sleep calls while waiting for the gitlab instance to start up

--- test/docker/init_data.rb
+++ test/docker/init_data.rb
@@ -1,9 +1,5 @@
 #!/usr/bin/env ruby
 u = User.first
-u.password_automatically_set = false 
-u.password = 'password' 
-u.password_confirmation = 'password' 
-u.save! 
 
 t = PersonalAccessToken.new({ user: u, name: 'node-gitlab', scopes: ['api']}) 
 t.save! 
