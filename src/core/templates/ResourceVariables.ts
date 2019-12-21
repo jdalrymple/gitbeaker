c7afca523160ac19707d0207b9892a81b799e645
Justin Dalrymple <jdalrymple@users.noreply.github.com>
2019-12-21 12:03:49 +0100

    fix: Typing on Group and Project variable keys was incorrect #512 (#515)
    closes: #512

--- src/core/templates/ResourceVariables.ts
+++ src/core/templates/ResourceVariables.ts
@@ -23,19 +23,19 @@ export class ResourceVariables extends BaseService {
     return RequestHelper.post(this, `${rId}/variables`, options);
   }
 
-  edit(resourceId: string | number, keyId: number, options?: BaseRequestOptions) {
+  edit(resourceId: string | number, keyId: string, options?: BaseRequestOptions) {
     const [rId, kId] = [resourceId, keyId].map(encodeURIComponent);
 
     return RequestHelper.put(this, `${rId}/variables/${kId}`, options);
   }
 
-  show(resourceId: string | number, keyId: number, options?: PaginatedRequestOptions) {
+  show(resourceId: string | number, keyId: string, options?: PaginatedRequestOptions) {
     const [rId, kId] = [resourceId, keyId].map(encodeURIComponent);
 
     return RequestHelper.get(this, `${rId}/variables/${kId}`, options);
   }
 
-  remove(resourceId: string | number, keyId: number, options?: PaginatedRequestOptions) {
+  remove(resourceId: string | number, keyId: string, options?: PaginatedRequestOptions) {
     const [rId, kId] = [resourceId, keyId].map(encodeURIComponent);
 
     return RequestHelper.del(this, `${rId}/variables/${kId}`, options);
