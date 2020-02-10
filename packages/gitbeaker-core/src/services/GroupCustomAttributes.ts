df21f7ff9a22edf0c89f282d1659a7c70bd3c530
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2020-02-10 18:56:20 +0000

    chore: Add interface for all resourcecustomattributes child classes

--- packages/gitbeaker-core/src/services/GroupCustomAttributes.ts
+++ packages/gitbeaker-core/src/services/GroupCustomAttributes.ts
@@ -1,5 +1,15 @@
 import { ResourceCustomAttributes } from '../templates';
-import { BaseServiceOptions } from '../infrastructure';
+import { BaseServiceOptions, PaginatedRequestOptions, Sudo } from '../infrastructure';
+
+export interface GroupCustomAttributes extends ResourceCustomAttributes {
+  all(groupId: string | number, options?: PaginatedRequestOptions);
+
+  set(groupId: string | number, customAttributeId: number, value: string, options?: Sudo);
+
+  remove(groupId: string | number, customAttributeId: number, options?: Sudo);
+
+  show(groupId: string | number, customAttributeId: number, options?: Sudo);
+}
 
 export class GroupCustomAttributes extends ResourceCustomAttributes {
   constructor(options: BaseServiceOptions = {}) {
