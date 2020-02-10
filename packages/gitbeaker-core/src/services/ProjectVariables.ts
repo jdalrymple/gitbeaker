f6c368a7bb5186a67e8731fe8f9424a34d3b5d3f
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2020-02-10 18:07:45 +0000

    chore: Add interface for all projectvariables child classes

--- packages/gitbeaker-core/src/services/ProjectVariables.ts
+++ packages/gitbeaker-core/src/services/ProjectVariables.ts
@@ -1,7 +1,24 @@
-import { ResourceVariables } from '../templates';
-import { BaseServiceOptions } from '../infrastructure';
+import { ResourceVariables, ResourceVariableSchema } from '../templates';
+import { BaseServiceOptions, PaginatedRequestOptions, BaseRequestOptions } from '../infrastructure';
 
-export { ResourceVariableSchema } from '../templates';
+export interface ProjectVariables extends ResourceVariables {
+  all(
+    projectId: string | number,
+    options?: PaginatedRequestOptions,
+  ): Promise<ResourceVariableSchema[]>;
+
+  create(projectId: string | number, options?: BaseRequestOptions);
+
+  edit(projectId: string | number, keyId: string, options?: BaseRequestOptions);
+
+  show(
+    projectId: string | number,
+    keyId: string,
+    options?: PaginatedRequestOptions,
+  ): Promise<ResourceVariableSchema>;
+
+  remove(projectId: string | number, keyId: string, options?: PaginatedRequestOptions);
+}
 
 export class ProjectVariables extends ResourceVariables {
   constructor(options: BaseServiceOptions = {}) {
