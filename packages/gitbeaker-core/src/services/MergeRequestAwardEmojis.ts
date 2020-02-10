5f6b20b6aa2e32acbb5934e15efb6ee3cd8a3ee6
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2020-02-10 19:05:58 +0000

    chore: Add interface for all resourceawardemojis child classes

--- packages/gitbeaker-core/src/services/MergeRequestAwardEmojis.ts
+++ packages/gitbeaker-core/src/services/MergeRequestAwardEmojis.ts
@@ -1,5 +1,38 @@
 import { ResourceAwardEmojis } from '../templates';
-import { BaseServiceOptions } from '../infrastructure';
+import { BaseServiceOptions, PaginatedRequestOptions, Sudo } from '../infrastructure';
+
+export interface MergeRequestAwardEmojis extends ResourceAwardEmojis {
+  all(
+    projectId: string | number,
+    mergerequestId: string | number,
+    noteId: number,
+    options?: PaginatedRequestOptions,
+  );
+
+  award(
+    projectId: string | number,
+    mergerequestId: string | number,
+    name: string,
+    noteId: number,
+    options?: Sudo,
+  );
+
+  remove(
+    projectId: string | number,
+    mergerequestId: string | number,
+    awardId: number,
+    noteId: number,
+    options?: Sudo,
+  );
+
+  show(
+    projectId: string | number,
+    mergerequestId: string | number,
+    awardId: number,
+    noteId: number,
+    options?: Sudo,
+  );
+}
 
 export class MergeRequestAwardEmojis extends ResourceAwardEmojis {
   constructor(options: BaseServiceOptions = {}) {
--- packages/gitbeaker-core/src/services/ProjectSnippetAwardEmojis.ts
+++ packages/gitbeaker-core/src/services/ProjectSnippetAwardEmojis.ts
@@ -1,5 +1,38 @@
 import { ResourceAwardEmojis } from '../templates';
-import { BaseServiceOptions } from '../infrastructure';
+import { BaseServiceOptions, PaginatedRequestOptions, Sudo } from '../infrastructure';
+
+export interface ProjectSnippetAwardEmojis extends ResourceAwardEmojis {
+  all(
+    projectId: string | number,
+    issueId: string | number,
+    noteId: number,
+    options?: PaginatedRequestOptions,
+  );
+
+  award(
+    projectId: string | number,
+    issueId: string | number,
+    name: string,
+    noteId: number,
+    options?: Sudo,
+  );
+
+  remove(
+    projectId: string | number,
+    issueId: string | number,
+    awardId: number,
+    noteId: number,
+    options?: Sudo,
+  );
+
+  show(
+    projectId: string | number,
+    issueId: string | number,
+    awardId: number,
+    noteId: number,
+    options?: Sudo,
+  );
+}
 
 export class ProjectSnippetAwardEmojis extends ResourceAwardEmojis {
   constructor(options: BaseServiceOptions = {}) {
