6ab30d2cffd642eb71b17f84a8a3a80210507a0b
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2020-02-10 18:09:02 +0000

    chore: Add interface for all resourcelabels child classes

--- packages/gitbeaker-core/src/services/Labels.ts
+++ packages/gitbeaker-core/src/services/Labels.ts
@@ -1,5 +1,29 @@
-import { BaseServiceOptions } from '../infrastructure';
+import {
+  BaseRequestOptions,
+  BaseServiceOptions,
+  PaginatedRequestOptions,
+  Sudo,
+} from '../infrastructure';
 import { ResourceLabels } from '../templates';
+
+export interface Labels extends ResourceLabels {
+  all(projectId: string | number, options?: PaginatedRequestOptions);
+
+  create(
+    projectId: string | number,
+    labelName: string,
+    color: string,
+    options?: BaseRequestOptions,
+  );
+
+  edit(projectId: string | number, labelName: string, options?: BaseRequestOptions);
+
+  remove(projectId: string | number, labelName: string, options?: Sudo);
+
+  subscribe(projectId: string | number, labelId: number, options?: Sudo);
+
+  unsubscribe(projectId: string | number, labelId: number, options?: Sudo);
+}
 
 export class Labels extends ResourceLabels {
   constructor(options: BaseServiceOptions = {}) {
