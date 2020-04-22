7a911266c75ec756e5a772ad5f6997285f75204f
Justin Dalrymple <jdalrymple@users.noreply.github.com>
2020-04-22 10:23:40 +0200

    Adding integration tests for the browser usage (#697)

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,6 @@
+const base = require('./jest.config.base');
+
 module.exports = {
-  preset: 'ts-jest',
-
-  testEnvironment: 'node',
-  testRegex: 'test\\/.*\\.ts$',
-  testMatch: null,
-  testURL: 'http://localhost/',
-  testTimeout: 15000,
-
-  coverageDirectory: 'coverage',
-  collectCoverage: true,
-
-  setupFilesAfterEnv: ['jest-extended'],
-
-  moduleNameMapper: {
-    '^ky$': require.resolve('ky').replace('index.js', 'umd.js'),
-  },
-
-  globals: {
-    'ts-jest': {
-      diagnostics: {
-        ignoreCodes: [2339],
-      },
-    },
-  },
+  ...base,
+  projects: ['<rootDir>', '<rootDir>/packages/*'],
 };
