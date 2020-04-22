6d184ad1edec4f5519290813237ecacbeb109bec
Justin <justin.s.dalrymple@gmail.com>
2020-04-22 20:38:37 +0200

    Fixing jest config

--- packages/gitbeaker-cli/jest.config.js
+++ packages/gitbeaker-cli/jest.config.js
@@ -7,5 +7,4 @@ module.exports = {
   globalSetup: 'jest-environment-puppeteer/setup',
   globalTeardown: 'jest-environment-puppeteer/teardown',
   testEnvironment: 'jest-environment-puppeteer',
-  displayName: 'Gitbeaker Browser',
 };
