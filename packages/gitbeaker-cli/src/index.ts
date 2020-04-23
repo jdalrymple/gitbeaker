3c34e5c3f06dfbdd12783ecc26aabde6e005a4a2
Justin <justin.s.dalrymple@gmail.com>
2020-04-23 20:46:07 +0200

    Fix missing alias support

--- packages/gitbeaker-cli/src/index.ts
+++ packages/gitbeaker-cli/src/index.ts
@@ -123,7 +123,7 @@ function runAPIMethod(args, apiName, method) {
 
     const camelCased = camelize(argName.replace('gb-', '').replace('gl-', ''), '-');
 
-    if (globalConfig[argName]) {
+    if (globalConfig[argName.replace('gl', 'gb')]) {
       initArgs[camelCased] = value;
     } else if (method.args.includes(camelCased)) coreArgs[camelCased] = value;
     else optionalArgs[camelCased] = value;
