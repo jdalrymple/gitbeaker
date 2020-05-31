5991ebe90d85a2521a81d0d9313cbde47aae2e6d
Justin <justin.s.dalrymple@gmail.com>
2020-05-31 16:27:59 +0200

    Moving over more tests

--- packages/gitbeaker-core/test/unit/services/IssuesStatistics.ts
+++ packages/gitbeaker-core/test/unit/services/IssuesStatistics.ts
@@ -35,26 +35,18 @@ describe('IssuesStatistics.all', () => {
   it('should request GET /issues_statistics', async () => {
     await service.all();
 
-    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'issues_statistics', undefined);
+    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'issues_statistics', {});
   });
 
   it('should request GET /projects/:id/issues_statistics when project Id is passed', async () => {
     await service.all({ projectId: 1 });
 
-    expect(RequestHelper.get).toHaveBeenCalledWith(
-      service,
-      'projects/1/issues_statistics',
-      undefined,
-    );
+    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'projects/1/issues_statistics', {});
   });
 
   it('should request GET /group/:id/issues_statistics when group Id is passed', async () => {
     await service.all({ groupId: 2 });
 
-    expect(RequestHelper.get).toHaveBeenCalledWith(
-      service,
-      'groups/1/issues_statistics',
-      undefined,
-    );
+    expect(RequestHelper.get).toHaveBeenCalledWith(service, 'groups/1/issues_statistics', {});
   });
 });
