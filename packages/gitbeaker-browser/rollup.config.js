e1e584087d7e35ab19baac16fdc27c87696447a5
Justin <justin.s.dalrymple@gmail.com>
2020-09-07 20:34:57 +0200

    Testing out esbuild

--- packages/gitbeaker-browser/rollup.config.js
+++ packages/gitbeaker-browser/rollup.config.js
@@ -1,27 +1,75 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import pkg from './package.json';
-import { terser } from 'rollup-plugin-terser';
+// import pkg from './package.json';
+// // import { terser } from 'rollup-plugin-terser';
 import { commonConfig, commonPlugins } from '../../rollup.config';
+// //
+// export default [
+//   {
+//     ...commonConfig,
+//     output: {
+//       file: pkg.browser,
+//       name: 'gitbeaker',
+//       format: 'iife',
+//       exports: 'named',
+//     },
+//     // external: ['react', 'react-dom'],
+//     // globals: {
+//     //   'react': 'React',
+//     //   'react-dom': 'ReactDOM'
+//     // },
+//     plugins: [
+//       globals(),
+//       resolve(),
+//       commonjs(),
+//       ...commonPlugins,
+//       //resolve(),
+//       // terser(),
+//     ],
+//   },
+// ];
+import polyfills from 'rollup-plugin-node-polyfills';
+import esbuild from 'rollup-plugin-esbuild';
+import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
 
-export default [
-  {
+const name = pkg.main.replace(/\.js$/, '');
+
+const ext = (format) => (format == 'dts' ? 'd.ts' : format == 'es' ? 'es.js' : 'js');
+
+function bundle(format) {
+  let plugins = [];
+
+  switch (format) {
+    case 'dts':
+      plugins.push(dts());
+      break;
+    case 'iife':
+      plugins.push(polyfills(), resolve(), commonjs(), esbuild());
+      break;
+    default:
+      plugins.push(esbuild());
+      break;
+  }
+
+  return {
     ...commonConfig,
     output: {
-      file: pkg.browser,
-      name: 'gitbeaker',
-      format: 'umd',
-      exports: 'named',
+      name: format == 'iife' ? 'gitbeaker' : undefined,
+      file: `${name}.${ext(format)}`,
+      format: format == 'dts' ? 'es' : format,
+      sourcemap: format != 'dts',
     },
-    plugins: [
-      globals(),
-      builtins(),
-      resolve({ browser: true }),
-      commonjs(),
-      ...commonPlugins,
-      terser(),
-    ],
-  },
+    plugins,
+    external: (id) => !/^[./]/.test(id),
+  };
+}
+
+console.log(bundle('iife'));
+
+export default [
+  // bundle('es'),
+  bundle('iife'),
+  // bundle('dts'),
 ];
--- packages/gitbeaker-requester-utils/rollup.config.js
+++ packages/gitbeaker-requester-utils/rollup.config.js
@@ -1,18 +1,43 @@
-import pkg from './package.json';
-import { commonConfig, commonPlugins } from '../../rollup.config';
+// import pkg from './package.json';
+// import { commonConfig, commonPlugins } from '../../rollup.config';
+//
+// export default {
+//   ...commonConfig,
+//   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+//   output: [
+//     {
+//       file: pkg.main, // CommonJS (for Node) (for bundlers) build.
+//       format: 'cjs',
+//       sourcemap: true,
+//     },
+//     {
+//       file: pkg.module, // ES module (for bundlers) build.
+//       format: 'es',
+//       sourcemap: true,
+//     },
+//   ],
+//   plugins: [
+//    ...commonPlugins,
+//   ],
+// };
 
-export default {
-  ...commonConfig,
-  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  output: [
-    {
-      file: pkg.main, // CommonJS (for Node) (for bundlers) build.
-      format: 'cjs',
-    },
-    {
-      file: pkg.module, // ES module (for bundlers) build.
-      format: 'es',
-    },
-  ],
-  plugins: commonPlugins,
-};
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import pkg from './package.json';
+
+const name = pkg.main.replace(/\.js$/, '');
+
+const ext = (format) => (format == 'dts' ? 'd.ts' : format == 'cjs' ? 'js' : 'es.js');
+
+const bundle = (format) => ({
+  input: 'src/index.ts',
+  output: {
+    file: `${name}.${ext(format)}`,
+    format: format == 'cjs' ? 'cjs' : 'es',
+    sourcemap: format != 'dts',
+  },
+  plugins: format == 'dts' ? [dts()] : [esbuild()],
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default [bundle('es'), bundle('cjs'), bundle('dts')];
