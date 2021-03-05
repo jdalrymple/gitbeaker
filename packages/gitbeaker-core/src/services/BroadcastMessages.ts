1402ba912cbb258e0e9a2e7eba634d095d715b0b
Justin <justin.s.dalrymple@gmail.com>
2021-03-04 22:10:01 -0500

    Adding types for application settings and broadcast message services

--- packages/gitbeaker-core/src/services/BroadcastMessages.ts
+++ packages/gitbeaker-core/src/services/BroadcastMessages.ts
@@ -4,32 +4,46 @@ import {
   PaginatedRequestOptions,
   RequestHelper,
   Sudo,
+  Camelize,
 } from '../infrastructure';
+
+export interface BroadcastMessageSchema extends Record<string, unknown> {
+  message: string;
+  starts_at: string;
+  ends_at: string;
+  color: string;
+  font: string;
+  id: number;
+  active: boolean;
+  target_path: string;
+  broadcast_type: string;
+  dismissable: boolean;
+}
 
 export class BroadcastMessages<C extends boolean = false> extends BaseService<C> {
   all(options?: PaginatedRequestOptions) {
-    return RequestHelper.get()(this, 'broadcast_messages', options);
+    return RequestHelper.get<BroadcastMessageSchema[]>()(this, 'broadcast_messages', options);
   }
 
-  create(options?: BaseRequestOptions) {
-    return RequestHelper.post()(this, 'broadcast_messages', options);
+  create(options?: Camelize<Omit<BroadcastMessageSchema, 'id'>> & Sudo) {
+    return RequestHelper.post<BroadcastMessageSchema>()(this, 'broadcast_messages', options);
   }
 
-  edit(broadcastMessageId: number, options?: BaseRequestOptions) {
+  edit(broadcastMessageId: number, options?: Camelize<Omit<BroadcastMessageSchema, 'id'>> & Sudo) {
     const bId = encodeURIComponent(broadcastMessageId);
 
-    return RequestHelper.put()(this, `broadcast_messages/${bId}`, options);
+    return RequestHelper.put<BroadcastMessageSchema>()(this, `broadcast_messages/${bId}`, options);
   }
 
   remove(broadcastMessageId: number, options?: Sudo) {
     const bId = encodeURIComponent(broadcastMessageId);
 
-    return RequestHelper.del()(this, `broadcast_messages/${bId}`, options);
+    return RequestHelper.del<void>()(this, `broadcast_messages/${bId}`, options);
   }
 
   show(broadcastMessageId: number, options?: BaseRequestOptions) {
     const bId = encodeURIComponent(broadcastMessageId);
 
-    return RequestHelper.get()(this, `broadcast_messages/${bId}`, options);
+    return RequestHelper.get<BroadcastMessageSchema>()(this, `broadcast_messages/${bId}`, options);
   }
 }
