c69689c5c0270d78fefe36b87304c59d0f1bc9fe
Justin <justin.s.dalrymple@gmail.com>
2021-03-09 22:48:40 -0500

    Add return type for all templates

--- packages/gitbeaker-core/src/services/LicenceTemplates.ts
+++ packages/gitbeaker-core/src/services/LicenceTemplates.ts
@@ -1,5 +1,29 @@
 import { BaseServiceOptions } from '@gitbeaker/requester-utils';
 import { ResourceTemplates } from '../templates';
+import {
+  PaginatedRequestOptions,
+  Sudo,
+  CamelizedRecord,
+} from '../infrastructure';
+
+export interface LicenceTemplateSchema extends Record<string, unknown> {
+  key: string;
+  name: string;
+  nickname?: null;
+  featured: boolean;
+  html_url: string;
+  source_url: string;
+  description: string;
+  conditions?: string[];
+  permissions?: string[];
+  limitations?: string[];
+  content: string;
+}
+
+export interface LicenceTemplates<C extends boolean = false> extends ResourceTemplates<C> {
+  all(options?: PaginatedRequestOptions): Promise<CamelizedRecord<C,LicenceTemplateSchema>[]>
+  show(key: string | number, options?: Sudo): Promise<CamelizedRecord<C,LicenceTemplateSchema>>
+}
 
 export class LicenceTemplates<C extends boolean = false> extends ResourceTemplates<C> {
   constructor(options: BaseServiceOptions<C> = {}) {
