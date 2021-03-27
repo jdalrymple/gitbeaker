127b47261fd4db45bdcf1e034b5942b19cc659ee
Justin <justin.s.dalrymple@gmail.com>
2021-03-27 19:56:36 -0400

    Push

--- packages/gitbeaker-node/src/GotRequester.ts
+++ packages/gitbeaker-node/src/GotRequester.ts
@@ -85,7 +85,7 @@ export async function handler(endpoint: string, options: Record<string, unknown>
         }
 
         console.log('HIT');
-        console.log(JSON.stringify(e.response, null, 2));
+        console.log(e.response);
 
         if (typeof e.response.body === 'string' && e.response.body.length > 0) {
           try {
