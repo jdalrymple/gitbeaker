7791478ef5a79b3d1a557f40ee4c5d1d9394045c
Justin <justin.s.dalrymple@gmail.com>
2021-05-04 00:48:36 -0400

    Adding more types

--- packages/gitbeaker-core/src/services/Snippets.ts
+++ packages/gitbeaker-core/src/services/Snippets.ts
@@ -1,4 +1,5 @@
 import { BaseService } from '@gitbeaker/requester-utils';
+import { UserSchema } from './Users';
 import {
   BaseRequestOptions,
   PaginatedRequestOptions,
@@ -8,11 +9,43 @@ import {
 
 export type SnippetVisibility = 'private' | 'public' | 'internal';
 
+export interface SnippetSchema extends Record<string, unknown> {
+  id: number;
+  title: string;
+  file_name: string;
+  description?: string;
+  visibility: string;
+  author: Pick<UserSchema, 'name' | 'username' | 'id' | 'state' | 'avatar_url' | 'web_url'>;
+  updated_at: string;
+  created_at: string;
+  project_id?: null;
+  web_url: string;
+  raw_url: string;
+}
+
+export interface FileSchema {
+  path: string;
+  raw_url: string;
+}
+
+export interface SnippetExpandedSchema extends SnippetSchema {
+  expires_at?: null;
+  ssh_url_to_repo: string,
+  http_url_to_repo: string,
+  files?: FileSchema[]
+}
+
+export interface UserAgentDetailSchema extends Record<string, unknown> {
+  user_agent: string;
+  ip_address: string;
+  akismet_submitted: boolean;
+}
+
 export class Snippets<C extends boolean = false> extends BaseService<C> {
   all({ public: p, ...options }: { public?: boolean } & PaginatedRequestOptions = {}) {
     const url = p ? 'snippets/public' : 'snippets';
 
-    return RequestHelper.get()(this, url, options);
+    return RequestHelper.get<SnippetSchema[]>()(this, url, options);
   }
 
   content(snippetId: number, options?: Sudo) {
@@ -28,7 +61,7 @@ export class Snippets<C extends boolean = false> extends BaseService<C> {
     visibility: SnippetVisibility,
     options?: BaseRequestOptions,
   ) {
-    return RequestHelper.post()(this, 'snippets', {
+    return RequestHelper.post<SnippetExpandedSchema>()(this, 'snippets', {
       title,
       fileName,
       content,
@@ -40,7 +73,7 @@ export class Snippets<C extends boolean = false> extends BaseService<C> {
   edit(snippetId: number, options?: BaseRequestOptions) {
     const sId = encodeURIComponent(snippetId);
 
-    return RequestHelper.put()(this, `snippets/${sId}`, options);
+    return RequestHelper.put<SnippetExpandedSchema>()(this, `snippets/${sId}`, options);
   }
 
   remove(snippetId: number, options?: Sudo) {
@@ -52,12 +85,12 @@ export class Snippets<C extends boolean = false> extends BaseService<C> {
   show(snippetId: number, options?: Sudo) {
     const sId = encodeURIComponent(snippetId);
 
-    return RequestHelper.get()(this, `snippets/${sId}`, options);
+    return RequestHelper.get<SnippetSchema>()(this, `snippets/${sId}`, options);
   }
 
   userAgentDetails(snippetId: number, options?: Sudo) {
     const sId = encodeURIComponent(snippetId);
 
-    return RequestHelper.get()(this, `snippets/${sId}/user_agent_detail`, options);
+    return RequestHelper.get<UserAgentDetailSchema>()(this, `snippets/${sId}/user_agent_detail`, options);
   }
 }
