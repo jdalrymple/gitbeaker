1906c9e3538326ca5f122e8efde684051d6f753b
Justin <justin.s.dalrymple@gmail.com>
2021-05-11 16:08:08 -0400

    Adding type updates for the Groups API
    - Adding support for the transfer projects endpoint

--- packages/gitbeaker-core/src/services/Groups.ts
+++ packages/gitbeaker-core/src/services/Groups.ts
@@ -2,13 +2,13 @@ import { BaseService } from '@gitbeaker/requester-utils';
 import {
   BaseRequestOptions,
   PaginatedRequestOptions,
+  ShowExpanded,
   RequestHelper,
   Sudo,
-  ShowExpanded,
 } from '../infrastructure';
 import { ProjectSchema } from './Projects';
 
-export interface GroupSchemaDefault {
+export interface GroupSchema extends Record<string, unknown> {
   id: number;
   name: string;
   path: string;
@@ -18,45 +18,65 @@ export interface GroupSchemaDefault {
   visibility: string;
   avatar_url: string;
   web_url: string;
+  description: string;
+  share_with_group_lock: boolean;
+  require_two_factor_authentication: boolean;
+  two_factor_grace_period: number;
+  project_creation_level: string;
+  auto_devops_enabled: boolean;
+  subgroup_creation_level: string;
+  emails_disabled: boolean;
+  mentions_disabled: boolean;
+  lfs_enabled: boolean;
+  default_branch_protection: number;
+  request_access_enabled: boolean;
+  file_template_project_id: number;
+  created_at: string;
 }
 
-export interface GroupSchemaCamelized {
+export type GroupDetailSchema = {
   id: number;
   name: string;
   path: string;
-  fullName: string;
-  fullPath: string;
-  parentId: number;
+  full_name: string;
+  full_path: string;
+  parent_id: number;
   visibility: string;
-  avatarUrl: string;
-  webUrl: string;
-}
-
-export type GroupSchema = GroupSchemaDefault | GroupSchemaCamelized;
-
-export type GroupDetailSchema = GroupSchema & {
-  projects: ProjectSchema[];
+  avatar_url: string;
+  web_url: string;
+  description: string;
+  request_access_enabled: boolean;
+  file_template_project_id: number;
+  runners_token: string;
+  shared_with_groups: {
+    group_id: number;
+    group_name: string;
+    group_full_path: string;
+    group_access_level: number;
+    expires_at: string;
+  }[];
+  created_at: string;
 };
 
-export class Groups extends BaseService {
-  all(options?: PaginatedRequestOptions): Promise<GroupSchema[]> {
-    return RequestHelper.get<GroupSchema>(this, 'groups', options) as Promise<GroupSchema[]>;
+export class Groups<C extends boolean = false> extends BaseService<C> {
+  all(options?: PaginatedRequestOptions) {
+    return RequestHelper.get<GroupSchema[]>()(this, 'groups', options);
   }
 
-  create(name: string, path: string, options?: BaseRequestOptions & ShowExpanded) {
-    return RequestHelper.post(this, 'groups', { name, path, ...options });
+  create(name: string, path: string, options?: BaseRequestOptions) {
+    return RequestHelper.post<GroupSchema>()(this, 'groups', { name, path, ...options });
   }
 
   createLDAPLink(
     groupId: string | number,
-    cn,
-    groupAccess,
+    cn: string,
+    groupAccess: number,
     provider: string,
     options?: Sudo & ShowExpanded,
   ) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.post(this, `groups/${gId}/ldap_group_links`, {
+    return RequestHelper.post()(this, `groups/${gId}/ldap_group_links`, {
       cn,
       groupAccess,
       provider,
@@ -64,63 +84,71 @@ export class Groups extends BaseService {
     });
   }
 
-  edit(groupId: string | number, options?: BaseRequestOptions & ShowExpanded) {
+  edit(groupId: string | number, options?: BaseRequestOptions) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.put(this, `groups/${gId}`, options);
+    return RequestHelper.put()(this, `groups/${gId}`, options);
   }
 
-  projects(groupId: string | number, options?: BaseRequestOptions): Promise<ProjectSchema[]> {
+  projects(groupId: string | number, options?: BaseRequestOptions) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.get<ProjectSchema>(this, `groups/${gId}/projects`, options) as Promise<
-      ProjectSchema[]
-    >;
+    return RequestHelper.get<ProjectSchema[]>()(this, `groups/${gId}/projects`, options);
   }
 
   remove(groupId: string | number, options?: Sudo & ShowExpanded) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.del(this, `groups/${gId}`, options);
+    return RequestHelper.del()(this, `groups/${gId}`, options);
   }
 
   removeLDAPLink(
     groupId: string | number,
-    cn,
+    cn: string,
     { provider, ...options }: Sudo & ShowExpanded & { provider?: string } = {},
   ) {
     const gId = encodeURIComponent(groupId);
     const url = provider ? `${provider}/${cn}` : `${cn}`;
 
-    return RequestHelper.del(this, `groups/${gId}/ldap_group_links/${url}`, options);
+    return RequestHelper.del()(
+      this,
+      `groups/${gId}/ldap_group_links/${url}`,
+      options as Record<string, unknown>,
+    );
   }
 
   search(nameOrPath: string, options?: Sudo) {
-    return RequestHelper.get(this, 'groups', {
+    return RequestHelper.get()(this, 'groups', {
       search: nameOrPath,
       ...options,
     });
   }
 
-  show(groupId: string | number, options?: BaseRequestOptions): Promise<GroupDetailSchema> {
+  show(groupId: string | number, options?: BaseRequestOptions) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.get<GroupDetailSchema>(
-      this,
-      `groups/${gId}`,
-      options,
-    ) as Promise<GroupDetailSchema>;
+    return RequestHelper.get<GroupDetailSchema>()(this, `groups/${gId}`, options);
   }
 
   subgroups(groupId: string | number, options?: PaginatedRequestOptions) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.get(this, `groups/${gId}/subgroups`, options);
+    return RequestHelper.get()(this, `groups/${gId}/subgroups`, options);
   }
 
   syncLDAP(groupId: string | number, options?: Sudo & ShowExpanded) {
     const gId = encodeURIComponent(groupId);
 
-    return RequestHelper.post(this, `groups/${gId}/ldap_sync`, options);
+    return RequestHelper.post()(this, `groups/${gId}/ldap_sync`, options);
+  }
+
+  transferProject(
+    groupId: string | number,
+    projectId: string | number,
+    options?: BaseRequestOptions & ShowExpanded,
+  ) {
+    const [gId, pId] = [groupId, projectId].map(encodeURIComponent);
+
+    return RequestHelper.post()(this, `groups/${gId}/projects/${pId}`, options);
   }
 }
