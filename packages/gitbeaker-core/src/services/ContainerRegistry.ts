9a54ab2c11ea2b24f33b377c14236157a0c31ce6
Justin <justin.s.dalrymple@gmail.com>
2021-05-11 15:57:52 -0400

    Adding type updates for the Container Registry API
    - Added showRepository function

--- packages/gitbeaker-core/src/services/ContainerRegistry.ts
+++ packages/gitbeaker-core/src/services/ContainerRegistry.ts
@@ -1,29 +1,80 @@
 import { BaseService } from '@gitbeaker/requester-utils';
 import { RequestHelper, PaginatedRequestOptions, Sudo } from '../infrastructure';
 
-export class ContainerRegistry extends BaseService {
-  repositories(projectId: string | number, options?: PaginatedRequestOptions) {
+export interface TagSchema extends Record<string, unknown> {
+  name: string;
+  path: string;
+  location: string;
+  revision: string;
+  short_revision: string;
+  digest: string;
+  created_at: string;
+  total_size: number;
+}
+
+export interface RepositorySchema extends Record<string, unknown> {
+  id: number;
+  name: string;
+  path: string;
+  project_id: number;
+  location: string;
+  created_at: string;
+  cleanup_policy_started_at: string;
+  tags_count?: number;
+  tags?: Pick<TagSchema, 'name' | 'path' | 'location'>[];
+}
+
+export class ContainerRegistry<C extends boolean = false> extends BaseService<C> {
+  projectRepositories(projectId: string | number, options?: PaginatedRequestOptions) {
     const pId = encodeURIComponent(projectId);
 
-    return RequestHelper.get(this, `projects/${pId}/registry/repositories`, options);
+    return RequestHelper.get<Omit<RepositorySchema, 'tags' | 'tags_count'>[]>()(
+      this,
+      `projects/${pId}/registry/repositories`,
+      options,
+    );
+  }
+
+  groupRepositories(projectId: string | number, options?: PaginatedRequestOptions) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.get<Omit<RepositorySchema, 'tags' | 'tags_count'>[]>()(
+      this,
+      `groups/${pId}/registry/repositories`,
+      options,
+    );
+  }
+
+  showRepository(projectId: string | number, repositoryId: number, options?: Sudo) {
+    const [pId, rId] = [projectId, repositoryId].map(encodeURIComponent);
+
+    return RequestHelper.get<RepositorySchema>()(
+      this,
+      `projects/${pId}/registry/repositories/${rId}`,
+      options,
+    );
   }
 
   tags(projectId: string | number, repositoryId: number, options?: PaginatedRequestOptions) {
     const [pId, rId] = [projectId, repositoryId].map(encodeURIComponent);
 
-    return RequestHelper.get(this, `projects/${pId}/registry/repositories/${rId}/tags`, options);
+    return RequestHelper.get<Pick<TagSchema, 'name' | 'path' | 'location'>[]>()(
+      this,
+      `projects/${pId}/registry/repositories/${rId}/tags`,
+      options,
+    );
   }
 
   removeRepository(projectId: string | number, repositoryId: number, options?: Sudo) {
     const [pId, rId] = [projectId, repositoryId].map(encodeURIComponent);
 
-    return RequestHelper.del(this, `projects/${pId}/registry/repositories/${rId}`, options);
+    return RequestHelper.del()(this, `projects/${pId}/registry/repositories/${rId}`, options);
   }
 
   removeTag(projectId: string | number, repositoryId: number, tagName: string, options?: Sudo) {
     const [pId, rId, tId] = [projectId, repositoryId, tagName].map(encodeURIComponent);
 
-    return RequestHelper.del(
+    return RequestHelper.del()(
       this,
       `projects/${pId}/registry/repositories/${rId}/tags/${tId}`,
       options,
@@ -38,7 +89,7 @@ export class ContainerRegistry extends BaseService {
   ) {
     const [pId, rId] = [projectId, repositoryId].map(encodeURIComponent);
 
-    return RequestHelper.del(this, `projects/${pId}/registry/repositories/${rId}/tags`, {
+    return RequestHelper.del()(this, `projects/${pId}/registry/repositories/${rId}/tags`, {
       nameRegexDelete,
       ...options,
     });
@@ -47,7 +98,7 @@ export class ContainerRegistry extends BaseService {
   showTag(projectId: string | number, repositoryId: number, tagName: string, options?: Sudo) {
     const [pId, rId, tId] = [projectId, repositoryId, tagName].map(encodeURIComponent);
 
-    return RequestHelper.get(
+    return RequestHelper.get<TagSchema>()(
       this,
       `projects/${pId}/registry/repositories/${rId}/tags/${tId}`,
       options,
