5329b3060a86a2bb5f0c5156f241725d4dc3f3d0
Justin <justin.s.dalrymple@gmail.com>
2021-05-11 15:59:28 -0400

    Adding type updates for the Feature Flags API
    Updating service support to include create, edit, show and remove

--- packages/gitbeaker-core/src/services/FeatureFlags.ts
+++ packages/gitbeaker-core/src/services/FeatureFlags.ts
@@ -1,14 +1,87 @@
 import { BaseService } from '@gitbeaker/requester-utils';
-import { BaseRequestOptions, PaginatedRequestOptions, RequestHelper } from '../infrastructure';
+import {
+  BaseRequestOptions,
+  PaginatedRequestOptions,
+  RequestHelper,
+  Sudo,
+} from '../infrastructure';
 
-export class FeatureFlags extends BaseService {
-  all(options?: PaginatedRequestOptions) {
-    return RequestHelper.get(this, 'features', options);
+export interface ScopeSchema {
+  id: number;
+  environment_scope: string;
+}
+export interface StrategySchema {
+  id: number;
+  name: string;
+  parameters: {
+    user_ids: string;
+  };
+  scopes?: ScopeSchema[];
+}
+
+export interface FeatureFlagSchema extends Record<string, unknown> {
+  name: string;
+  description: string;
+  active: boolean;
+  version: string;
+  created_at: string;
+  updated_at: string;
+  scopes?: string[];
+  strategies?: StrategySchema[];
+}
+
+export class FeatureFlags<C extends boolean = false> extends BaseService<C> {
+  all(
+    projectId: string | number,
+    options: { scopes?: 'enabled' | 'disabled' } & PaginatedRequestOptions = {},
+  ) {
+    const pId = encodeURIComponent(projectId);
+
+    return RequestHelper.get<FeatureFlagSchema[]>()(
+      this,
+      `projects/${pId}/features_flags`,
+      options,
+    );
   }
 
-  set(name: string, options?: BaseRequestOptions) {
-    const encodedName = encodeURIComponent(name);
+  create(
+    projectId: string | number,
+    flagName: string,
+    version: string,
+    options?: BaseRequestOptions,
+  ) {
+    const [pId, fName, ver] = [projectId, flagName, version].map(encodeURIComponent);
 
-    return RequestHelper.post(this, `features/${encodedName}`, options);
+    return RequestHelper.post<FeatureFlagSchema>()(this, `projects/${pId}/features_flags`, {
+      version: ver,
+      name: fName,
+      ...options,
+    });
+  }
+
+  edit(projectId: string | number, flagName: string, options?: BaseRequestOptions) {
+    const [pId, fName] = [projectId, flagName].map(encodeURIComponent);
+
+    return RequestHelper.put<FeatureFlagSchema>()(
+      this,
+      `projects/${pId}/features_flags/${fName}`,
+      options,
+    );
+  }
+
+  remove(projectId: string | number, flagName: string, options?: Sudo) {
+    const [pId, fName] = [projectId, flagName].map(encodeURIComponent);
+
+    return RequestHelper.del()(this, `projects/${pId}/features_flags/${fName}`, options);
+  }
+
+  show(projectId: string | number, flagName: string, options?: Sudo) {
+    const [pId, fName] = [projectId, flagName].map(encodeURIComponent);
+
+    return RequestHelper.get<FeatureFlagSchema>()(
+      this,
+      `projects/${pId}/features_flags/${fName}`,
+      options,
+    );
   }
 }
