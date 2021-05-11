98b840431d2c6e7d81ab06684b5429830c55ddea
Justin <justin.s.dalrymple@gmail.com>
2021-05-11 16:07:08 -0400

    Adding type updates for the License Templates API
    - Renamed the export to fix the spelling error

---
+++ packages/gitbeaker-core/src/services/LicenseTemplates.ts
@@ -0,0 +1,29 @@
+import { BaseServiceOptions } from '@gitbeaker/requester-utils';
+import { ResourceTemplates } from '../templates';
+import { PaginatedRequestOptions, Sudo, CamelizedRecord } from '../infrastructure';
+
+export interface LicenseTemplateSchema extends Record<string, unknown> {
+  key: string;
+  name: string;
+  nickname?: string;
+  featured: boolean;
+  html_url: string;
+  source_url: string;
+  description: string;
+  conditions?: string[];
+  permissions?: string[];
+  limitations?: string[];
+  content: string;
+}
+
+export interface LicenseTemplates<C extends boolean = false> extends ResourceTemplates<C> {
+  all(options?: PaginatedRequestOptions): Promise<CamelizedRecord<C, LicenseTemplateSchema>[]>;
+  show(key: string | number, options?: Sudo): Promise<CamelizedRecord<C, LicenseTemplateSchema>>;
+}
+
+export class LicenseTemplates<C extends boolean = false> extends ResourceTemplates<C> {
+  constructor(options: BaseServiceOptions<C>) {
+    /* istanbul ignore next */
+    super('Licenses', options);
+  }
+}
