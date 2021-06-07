b76bf3a8730c5cb4ff08d234e037ccabdcb9621e
Justin <justin.s.dalrymple@gmail.com>
2021-06-07 15:22:40 -0400

    Fixing integration tests

--- packages/cli/test/integration/services/Projects.ts
+++ packages/cli/test/integration/services/Projects.ts
@@ -1,21 +1,16 @@
-import { spawnSync } from 'child_process';
+import { promisify } from 'util'
+import { exec } from 'child_process';
+
+const execP = promisify(exec)
 
 const { TEST_ID = '', GITLAB_PERSONAL_ACCESS_TOKEN = '', GITLAB_URL = '' } = process.env;
 
 describe('gitbeaker projects create', () => {
-  it('should create a valid project', () => {
-    const output = spawnSync('node', [
-      '../../../dist/index',
-      `gitbeaker projects create --name="CLI Project ${TEST_ID}" --gb-token="${GITLAB_PERSONAL_ACCESS_TOKEN}" --gb-host="${GITLAB_URL}"`,
-    ]);
+  it('should create a valid project', async () => {
+    const command = `projects create --name="CLI Project ${TEST_ID}" --gb-token="${GITLAB_PERSONAL_ACCESS_TOKEN}" --gb-host="${GITLAB_URL}"`;
+    const { stdout } = await execP(`node dist/index.js ${command}`)
+    const { name } = JSON.parse(stdout)
 
-    console.log('error', output.error);
-    console.log('stdout ', output.stdout);
-    console.log('stderr ', output.stderr);
-    console.log('stderr ', output.output);
-
-    console.log(JSON.stringify(output));
-    expect(true).toBeTruthy();
-    // expect(data.name).toBe(`CLI Project ${TEST_ID}`);
+    expect(name).toBe(`CLI Project ${TEST_ID}`);
   });
 });
