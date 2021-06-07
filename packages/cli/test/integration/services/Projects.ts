c792bc31e13f5a04cb02872fd69ff9f7264b67a0
Justin Dalrymple <jdalrymple@users.noreply.github.com>
2021-06-07 15:38:59 -0400

    Fix CLI release compilation  (#1838)
    fixes: #1676

--- packages/cli/test/integration/services/Projects.ts
+++ packages/cli/test/integration/services/Projects.ts
@@ -1,14 +1,16 @@
+import { promisify } from 'util';
+import { exec } from 'child_process';
+
+const execP = promisify(exec);
+
 const { TEST_ID = '', GITLAB_PERSONAL_ACCESS_TOKEN = '', GITLAB_URL = '' } = process.env;
 
 describe('gitbeaker projects create', () => {
   it('should create a valid project', async () => {
-    // eslint-disable-next-line
-    const { cli } = require('../../../dist/index');
-    const { output } = await cli.parse(
-      `gitbeaker projects create --name="CLI Project ${TEST_ID}" --gb-token="${GITLAB_PERSONAL_ACCESS_TOKEN}" --gb-host="${GITLAB_URL}"`,
-    );
-    const data = JSON.parse(output);
+    const command = `projects create --name="CLI Project ${TEST_ID}" --gb-token="${GITLAB_PERSONAL_ACCESS_TOKEN}" --gb-host="${GITLAB_URL}"`;
+    const { stdout } = await execP(`node dist/index.js ${command}`);
+    const { name } = JSON.parse(stdout);
 
-    expect(data.name).toBe(`CLI Project ${TEST_ID}`);
+    expect(name).toBe(`CLI Project ${TEST_ID}`);
   });
 });
