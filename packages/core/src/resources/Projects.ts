4f2166210b7b6fbfaa766c05a15c2435a8bab293
Aliyss Snow <33941859+Aliyss@users.noreply.github.com>
2021-07-27 07:07:06 -0700

    Added Topics to ProjectSchema & Updated Commit.ts (#1932)
    - Added Topics and namespace to ProjectSchema
    - Made ref optional in RepositoryFiles.ts
    - Updating CommitAction types
    
    Co-authored-by: Justin Dalrymple <jdalrymple@users.noreply.github.com>

--- packages/core/src/resources/Projects.ts
+++ packages/core/src/resources/Projects.ts
@@ -21,8 +21,13 @@ export interface ProjectSchema extends Record<string, unknown> {
   web_url: string;
   readme_url: string;
   tag_list?: string[];
+  topics?: string[];
   name: string;
   name_with_namespace: string;
+  namespace: Pick<
+    NamespaceSchema,
+    'id' | 'name' | 'path' | 'kind' | 'full_path' | 'avatar_url' | 'web_url'
+  >;
   path: string;
   path_with_namespace: string;
   created_at: string;
@@ -66,10 +71,6 @@ export interface ProjectExtendedSchema extends ProjectSchema {
     next_run_at: string;
   };
   creator_id: number;
-  namespace: Pick<
-    NamespaceSchema,
-    'id' | 'name' | 'path' | 'kind' | 'full_path' | 'avatar_url' | 'web_url'
-  >;
   import_status: string;
   import_error?: string;
   permissions: {
