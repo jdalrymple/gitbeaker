bed926b54e8e5ac85240f5c2348522ef1ee22104
Justin <justin.s.dalrymple@gmail.com>
2021-08-27 15:31:16 -0400

    Adding a core integration test, and fixing the resource imports
    Removed artefact imports during builds

--- packages/core/test/integration/infratructure/Utils.ts
+++ packages/core/test/integration/infratructure/Utils.ts
@@ -1,13 +1,13 @@
 import 'jest-extended';
 import * as Core from '../../../src';
 
-const { Types, getAPIMap, Gitlab, ...resources } = Core;
+const { getAPIMap, Resources } = Core;
 
 describe('getAPIMap', () => {
   it('should return object representing all resources in JSON format', () => {
     const map = getAPIMap();
 
     expect(map).toBeInstanceOf(Object);
-    expect(Object.keys(map)).toIncludeAllMembers(Object.keys(resources));
+    expect(Object.keys(map)).toIncludeAllMembers(Object.keys(Resources));
   });
 });
