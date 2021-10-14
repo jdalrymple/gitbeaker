25d78e7bd384f6c6fc676265bac497dc9d72a4ef
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2021-10-14 12:08:51 -0400

    Print out some more info

--- packages/node/test/integration/resources/Projects.ts
+++ packages/node/test/integration/resources/Projects.ts
@@ -39,19 +39,24 @@ describe('Projects.all', () => {
   });
 });
 
-// describe.skip('Projects.upload', () => {
-//   it('should upload a text file', async () => {
-//     const project = await service.create({
-//       name: `Project Upload Integration Test Text File ${TEST_ID}`,
-//     });
-//
-//     const results = await service.upload(project.id, 'TESTING FILE UPLOAD', {
-//       metadata: {
-//         filename: 'testfile.txt',
-//         contentType: 'text/plain',
-//       },
-//     });
-//
-//     expect(results).toContainKeys(['alt', 'url', 'full_path', 'markdown']);
-//   });
-// });
+describe.skip('Projects.upload', () => {
+  it('should upload a text file', async () => {
+    const project = await service.create({
+      name: `Project Upload Integration Test Text File ${TEST_ID}`,
+    });
+
+    try {
+    const results = await service.upload(project.id, 'TESTING FILE UPLOAD', {
+      metadata: {
+        filename: 'testfile.txt',
+        contentType: 'text/plain',
+      },
+    });
+
+    expect(results).toContainKeys(['alt', 'url', 'full_path', 'markdown']);
+  } catch(e) {
+    console.log(JSON.stringify(e, null, 2))
+    console.log(e.response)
+  }
+  });
+});
