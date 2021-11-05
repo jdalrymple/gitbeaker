bfc5cbd6431cee166e564abd0a0c6cf773208145
Martin Howarth <MartinHowarth@users.noreply.github.com>
2021-11-05 23:23:22 +0000

    Add ability to request merged_yaml from the Gitlab Lint API (#2185)

--- packages/core/src/resources/Lint.ts
+++ packages/core/src/resources/Lint.ts
@@ -1,14 +1,15 @@
 import { BaseResource } from '@gitbeaker/requester-utils';
-import { RequestHelper, Sudo } from '../infrastructure';
+import { BaseRequestOptions, RequestHelper } from '../infrastructure';
 
 export interface LintSchema extends Record<string, unknown> {
   status: string;
   errors?: string[];
   warnings?: string[];
+  merged_yaml?: string;
 }
 
 export class Lint<C extends boolean = false> extends BaseResource<C> {
-  lint(content: string, options?: Sudo) {
+  lint(content: string, options?: BaseRequestOptions) {
     return RequestHelper.post<LintSchema>()(this, 'ci/lint', { content, ...options });
   }
 }
