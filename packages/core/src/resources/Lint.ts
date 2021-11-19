d8fefc82f4db6f19e179ff193605e1bcc2a2ba48
Martin Howarth <howarth.martin@gmail.com>
2021-11-19 18:39:29 +0000

    Add support for CI lint with namespace context

--- packages/core/src/resources/Lint.ts
+++ packages/core/src/resources/Lint.ts
@@ -7,9 +7,28 @@ export interface LintSchema extends Record<string, unknown> {
   warnings?: string[];
   merged_yaml?: string;
 }
+export interface LintWithNamespaceSchema extends Record<string, unknown> {
+  valid: boolean;
+  merged_yaml: string;
+  errors?: string[];
+  warnings?: string[];
+}
 
 export class Lint<C extends boolean = false> extends BaseResource<C> {
   lint(content: string, options?: BaseRequestOptions) {
+    // Perform CI file linting without context.
+    // See https://docs.gitlab.com/ee/api/lint.html#validate-the-ci-yaml-configuration
+    // This API doesn't work for CI files that contain `local` includes. Use `lint_with_namespace` instead.
     return RequestHelper.post<LintSchema>()(this, 'ci/lint', { content, ...options });
+  }
+
+  lint_with_namespace(projectId: string | number, content: string, options?: BaseRequestOptions) {
+    // Perform CI file linting in the context of a specific project namespace.    
+    // See https://docs.gitlab.com/ee/api/lint.html#validate-a-ci-yaml-configuration-with-a-namespace
+    // This API is useful when the CI file being linted has `local` includes, which requires project
+    // context to be understood.
+    return RequestHelper.post<LintWithNamespaceSchema>()(this, 
+      `projects/${projectId}/ci/lint`,
+      { content, ...options });
   }
 }
--- packages/node/test/integration/resources/Lint.ts
+++ packages/node/test/integration/resources/Lint.ts
@@ -1,10 +1,17 @@
 import 'jest-extended';
-import { Lint } from '../../../src';
+import { Lint, Projects } from '../../../src';
 
+const { TEST_ID = '' } = process.env;
+let projectAPI: InstanceType<typeof Projects>;
 let lintAPI: InstanceType<typeof Lint>;
+
 
 beforeEach(() => {
   lintAPI = new Lint({
+    host: process.env.GITLAB_URL,
+    token: process.env.GITLAB_PERSONAL_ACCESS_TOKEN,
+  });
+  projectAPI = new Projects({
     host: process.env.GITLAB_URL,
     token: process.env.GITLAB_PERSONAL_ACCESS_TOKEN,
   });
@@ -37,5 +44,27 @@ describe('Lint.lint', () => {
 
     expect(result).toBeInstanceOf(Object);
     expect(result).toContainKeys(['status', 'merged_yaml']);
+  });
+});
+
+describe('Lint.lint_with_namespace', () => {
+  it('should return the merged yaml in a lint request when requested', async () => {
+    // Call the lint API for a project namespace, passing in a basic CI yaml.
+    // The specific use case for `lint_with_namespace` vs `lint` is that it works with `local`
+    // includes. However, that's gitlab-side functionality so we're not testing that here - 
+    // simply aiming to ensure that we can call this API correctly.
+
+    const project = await projectAPI.create({ name: `Lint API Test ${TEST_ID}` });
+
+    const input_ci_yaml = `
+    test:
+      stage: test
+      script:
+        - echo 1
+    `;
+    const result = await lintAPI.lint_with_namespace(project.id, input_ci_yaml);
+
+    expect(result).toBeInstanceOf(Object);
+    expect(result).toContainKeys(['valid', 'merged_yaml']);
   });
 });
