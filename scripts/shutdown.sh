cc4b7b5a40ac56279118721e7e217be596f324bd
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2024-01-25 02:23:22 -0500

    Tinkering with shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -17,5 +17,11 @@ fi
 toolbox apt-get install at
 
 # schedule the instance to delete itself
-echo "gcloud compute instances delete $(hostname) --zone \
-$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4) -q" | at Now + $TIMEOUT Minutes
+INSTANCE_NAME=$(curl -sq "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
+INSTANCE_ZONE=$(curl -sq "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
+
+echo "Terminating instance [${INSTANCE_NAME}] in zone [${INSTANCE_ZONE}}"
+
+TOKEN=$(curl -sq "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" | jq -r '.access_token')
+
+echo 'curl -X DELETE -H "Authorization: Bearer ${TOKEN}" https://www.googleapis.com/compute/v1/$INSTANCE_ZONE/instances/$INSTANCE_NAME' | at Now + $TIMEOUT Minutes
