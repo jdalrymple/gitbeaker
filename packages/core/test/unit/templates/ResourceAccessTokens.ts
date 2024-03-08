761940234d263596a8a73320abd60f88c63f0bf7
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2024-03-08 08:53:40 -0500

    Updating tests

--- packages/core/test/unit/templates/ResourceAccessTokens.ts
+++ packages/core/test/unit/templates/ResourceAccessTokens.ts
@@ -43,7 +43,8 @@ describe('ResourceAccessTokens.all', () => {
 
 describe('ResourceAccessTokens.create', () => {
   it('should call the correct url for creating access token with a string identifer', async () => {
-    await service.create('5', 'test', ['api']);
+    await service.create('5', 'test', ['api'], '2021-01-31');
+
     expect(RequestHelper.post()).toHaveBeenCalledWith(service, '5/access_tokens', {
       name: 'test',
       scopes: ['api'],
@@ -51,7 +52,8 @@ describe('ResourceAccessTokens.create', () => {
   });
 
   it('should call the correct url for creating access token with a number identifer', async () => {
-    await service.create(5, 'test', ['api']);
+    await service.create(5, 'test', ['api'], '2021-01-31');
+
     expect(RequestHelper.post()).toHaveBeenCalledWith(service, '5/access_tokens', {
       name: 'test',
       scopes: ['api'],
@@ -62,23 +64,57 @@ describe('ResourceAccessTokens.create', () => {
 describe('ResourceAccessTokens.show', () => {
   it('should call the correct url with a string identifer', async () => {
     await service.show('5', '6');
+
     expect(RequestHelper.get()).toHaveBeenCalledWith(service, '5/access_tokens/6', undefined);
   });
 
   it('should call the correct url for creating access token with a number identifer', async () => {
     await service.show(5, 6);
+
     expect(RequestHelper.get()).toHaveBeenCalledWith(service, '5/access_tokens/6', undefined);
+  });
+});
+
+describe('ResourceAccessTokens.rotate', () => {
+  it('should call the correct url with a string identifer', async () => {
+    await service.rotate('5', '6');
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(
+      service,
+      '5/access_tokens/6/rotate',
+      undefined,
+    );
+  });
+
+  it('should call the correct url for creating access token with a number identifer', async () => {
+    await service.rotate(5, 6);
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(
+      service,
+      '5/access_tokens/6/rotate',
+      undefined,
+    );
+  });
+
+  it('should take options', async () => {
+    const options = { expiresAt: '2021-01-31' };
+
+    await service.rotate('5', '6', options);
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(service, '5/access_tokens/6/rotate', options);
   });
 });
 
 describe('ResourceAccessTokens.revoke', () => {
   it('should call the correct url with a string identifer', async () => {
     await service.revoke('5', '6');
+
     expect(RequestHelper.del()).toHaveBeenCalledWith(service, '5/access_tokens/6', undefined);
   });
 
   it('should call the correct url for creating access token with a number identifer', async () => {
     await service.revoke(5, 6);
+
     expect(RequestHelper.del()).toHaveBeenCalledWith(service, '5/access_tokens/6', undefined);
   });
 });
