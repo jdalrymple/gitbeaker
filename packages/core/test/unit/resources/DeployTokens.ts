8792c2c7e4abd4606843905f4814c283d3140927
mpsanchis <miguelpsanchis@gmail.com>
2024-10-30 10:55:03 +0100

    Add options `username` and `expires_at` to DeployTokens.create

--- packages/core/test/unit/resources/DeployTokens.ts
+++ packages/core/test/unit/resources/DeployTokens.ts
@@ -16,12 +16,6 @@ beforeEach(() => {
 });
 
 describe('DeployTokens.create', () => {
-  it('should throw an error when attempting to create a deploy_key without specifying the groupId or projectId', () => {
-    expect(() => service.create('token', ['read_repository'])).toThrow(
-      'Missing required argument. Please supply a projectId or a groupId in the options parameter.',
-    );
-  });
-
   it('should request POST groups/5/deploy_tokens with a name and scopes', async () => {
     await service.create('token', ['read_repository'], { groupId: 5 });
 
@@ -37,6 +31,29 @@ describe('DeployTokens.create', () => {
     expect(RequestHelper.post()).toHaveBeenCalledWith(service, 'projects/5/deploy_tokens', {
       name: 'token',
       scopes: ['read_repository'],
+    });
+  });
+
+  it('should request POST projects/5/deploy_tokens with name, scopes, and username if passed', async () => {
+    await service.create('token', ['read_repository'], { projectId: 5, username: 'mr-smith' });
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(service, 'projects/5/deploy_tokens', {
+      name: 'token',
+      scopes: ['read_repository'],
+      username: 'mr-smith',
+    });
+  });
+
+  it('should request POST projects/5/deploy_tokens with name, scopes, and date if passed', async () => {
+    await service.create('token', ['read_repository'], {
+      projectId: 5,
+      expires_at: '1975-08-19T22:15:30.000Z',
+    });
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(service, 'projects/5/deploy_tokens', {
+      name: 'token',
+      scopes: ['read_repository'],
+      expires_at: '1975-08-19T22:15:30.000Z',
     });
   });
 });
