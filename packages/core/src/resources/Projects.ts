83112b6e13240cfb52a3a1b015381cd363125df8
Zack <42388953+zk-kb4@users.noreply.github.com>
2025-04-18 15:50:11 -0400

    Add invited_groups API request for projects

--- packages/core/src/resources/Projects.ts
+++ packages/core/src/resources/Projects.ts
@@ -511,6 +511,23 @@ export class Projects<C extends boolean = false> extends BaseResource<C> {
     );
   }
 
+	allSharedGroups<E extends boolean = false>(
+		projectId: string | number,
+		options?: {
+			search?: string;
+			sharedMinAccessLevel?: Exclude<AccessLevel, AccessLevel.ADMIN>;
+			relation?: 'direct' | 'inherited';
+			withCustomAttributes: true;
+		} & Sudo &
+			ShowExpanded<E>,
+	): Promise<GitlabAPIResponse<SimpleGroupSchema[], C, E, void>> {
+		return get<SimpleGroupSchema[]>()(
+			this,
+			endpoint`projects/${projectId}/invited_groups`,
+			options,
+		);
+	}
+
   allSharableGroups<E extends boolean = false>(
     projectId: string | number,
     options?: {
