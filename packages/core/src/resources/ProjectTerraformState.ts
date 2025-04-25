48bd6937b53d3aa2779b06e58c5576294dc8b243
Zack Knight <zachk@knowbe4.com>
2025-04-25 14:47:39 -0400

    Add Terraform State API

---
+++ packages/core/src/resources/ProjectTerraformState.ts
@@ -0,0 +1,89 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import { GitlabAPIResponse, ShowExpanded, Sudo } from '@gitbeaker/core';
+import { endpoint, RequestHelper } from '../infrastructure';
+
+export interface TerraformStateSchema extends Record<string, unknown> {
+  version: number;
+  terraform_version: string;
+  serial: number;
+  lineage: string;
+  outputs?: Record<string, unknown>;
+  resources?: Record<string, unknown>[];
+  check_results: Record<string, unknown>[];
+}
+
+export class ProjectTerraformState<C extends boolean = false> extends BaseResource<C> {
+  show<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<TerraformStateSchema, C, E, void>> {
+    return RequestHelper.get<TerraformStateSchema>()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}`,
+      options,
+    );
+  }
+
+  showVersion<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    serial: number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<TerraformStateSchema, C, E, void>> {
+    return RequestHelper.get<TerraformStateSchema>()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}/versions/${serial}`,
+      options,
+    );
+  }
+
+  removeVersion<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    serial: number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<void, C, E, void>> {
+    return RequestHelper.del()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}/versions/${serial}`,
+      options,
+    );
+  }
+
+  remove<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<void, C, E, void>> {
+    return RequestHelper.del()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}`,
+      options,
+    );
+  }
+
+  removeTerraformStateLock<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<void, C, E, void>> {
+    return RequestHelper.del()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}/lock`,
+      options,
+    );
+  }
+
+  createVersion<E extends boolean = false>(
+    projectId: string | number,
+    name: string,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<TerraformStateSchema, C, E, void>> {
+    return RequestHelper.post<TerraformStateSchema>()(
+      this,
+      endpoint`projects/${projectId}/terraform/state/${name}`,
+      options,
+    );
+  }
+}
