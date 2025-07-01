7daf7adec5093224d6291324acf6b1d827bf841f
aiab <aidinabedi@gmail.com>
2025-07-01 17:14:49 +0200

    Add support for markdown uploads

---
+++ packages/core/src/resources/GroupMarkdownUploads.ts
@@ -0,0 +1,35 @@
+import type { BaseResourceOptions } from '@gitbeaker/requester-utils';
+import { MarkdownUploadSchema, ResourceMarkdownUploads } from '../templates';
+import type {
+  GitlabAPIResponse,
+  PaginationRequestOptions,
+  PaginationTypes,
+  Sudo,
+} from '../infrastructure';
+
+export interface GroupMarkdownUploads<C extends boolean = false>
+  extends ResourceMarkdownUploads<C> {
+  all<E extends boolean = false, P extends PaginationTypes = 'offset'>(
+    groupId: string | number,
+    options?: Sudo & PaginationRequestOptions<P>,
+  ): Promise<GitlabAPIResponse<MarkdownUploadSchema[], C, E, P>>;
+
+  download<E extends boolean = false>(
+    groupId: string | number,
+    uploadId: string | number,
+    options?: Sudo,
+  ): Promise<GitlabAPIResponse<Blob, C, E, void>>;
+
+  remove<E extends boolean = false>(
+    groupId: string | number,
+    uploadId: string | number,
+    options?: Sudo,
+  ): Promise<GitlabAPIResponse<void, C, E, void>>;
+}
+
+export class GroupMarkdownUploads<C extends boolean = false> extends ResourceMarkdownUploads<C> {
+  constructor(options: BaseResourceOptions<C>) {
+    /* istanbul ignore next */
+    super('groups', options);
+  }
+}
---
+++ packages/core/test/unit/resources/ProjectMarkdownUploads.ts
@@ -0,0 +1,41 @@
+import { ProjectMarkdownUploads } from '../../../src/resources';
+import { RequestHelper } from '../../../src/infrastructure';
+
+jest.mock(
+  '../../../src/infrastructure/RequestHelper',
+  () => require('../../__mocks__/RequestHelper').default,
+);
+
+let service: ProjectMarkdownUploads<false>;
+
+beforeEach(() => {
+  service = new ProjectMarkdownUploads({
+    requesterFn: jest.fn(),
+    token: 'abcdefg',
+  });
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Instantiating ProjectMarkdownUploads service', () => {
+  it('should create a valid service object', () => {
+    expect(service).toBeInstanceOf(ProjectMarkdownUploads);
+    expect(service.url).toBeDefined();
+    expect(service.url).toContain('projects');
+  });
+});
+
+describe('ProjectMarkdownUploads.create', () => {
+  it('should call the correct url with a resource id and formdata', async () => {
+    const content = new Blob(['content'], { type: 'text/plain' });
+    const file = { content, filename: 'content.txt' };
+    await service.create('5', file);
+
+    expect(RequestHelper.post()).toHaveBeenCalledWith(service, '5/uploads', {
+      isForm: true,
+      file: [file.content, file.filename],
+    });
+  });
+});
