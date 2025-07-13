85952e67edb8807cbad4cc0f021369466f38d804
Bryan Lee <38807139+liby@users.noreply.github.com>
2025-07-13 11:51:03 +0800

    Update AcceptMergeRequestOptions type definition (#3734)

--- packages/core/src/resources/MergeRequests.ts
+++ packages/core/src/resources/MergeRequests.ts
@@ -239,8 +239,12 @@ export type AcceptMergeRequestOptions = {
   squashCommitMessage?: string;
   squash?: boolean;
   shouldRemoveSourceBranch?: boolean;
+  /**
+   * @deprecated Use `autoMerge` instead. Deprecated in GitLab 17.11.
+   */
   mergeWhenPipelineSucceeds?: boolean;
   sha?: string;
+  autoMerge?: boolean;
 };
 
 export type EditMergeRequestOptions = {
