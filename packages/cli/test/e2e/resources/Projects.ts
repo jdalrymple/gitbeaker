36ef1d9e799b5d19f3e302d1b871bf71d25b04df
Justin Dalrymple <justin.dalrymple@dialogue.co>
2026-01-21 10:23:49 -0500

    Force the type

--- packages/cli/test/e2e/resources/Projects.ts
+++ packages/cli/test/e2e/resources/Projects.ts
@@ -1,7 +1,7 @@
 import { promisify } from 'node:util';
 import { exec } from 'node:child_process';
 
-const execP = promisify(exec);
+const execP: (command: string) => Promise<{ stdout: string }> = promisify(exec);
 
 const { GITLAB_PERSONAL_ACCESS_TOKEN = '', GITLAB_URL = '', TEST_ID = Date.now() } = process.env;
 
@@ -9,7 +9,7 @@ describe('gitbeaker projects create', () => {
   it('should create a valid project', async () => {
     const command = `projects create --name="CLI Project - e2e ${TEST_ID}" --gb-token="${GITLAB_PERSONAL_ACCESS_TOKEN}" --gb-host="${GITLAB_URL}"`;
     const { stdout } = await execP(`node dist/index.mjs ${command}`);
-    const { name } = JSON.parse(stdout as string);
+    const { name } = JSON.parse(stdout.toString());
 
     expect(name).toBe(`CLI Project - e2e ${TEST_ID}`);
   });
