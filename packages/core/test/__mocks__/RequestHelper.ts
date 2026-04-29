8d14814131dc05212b949c097626589c866bb6ee
Justin Dalrymple <justin.dalrymple@dialogue.co>
2026-04-29 00:56:54 -0400

    Fixing testS

--- packages/core/test/__mocks__/RequestHelper.ts
+++ packages/core/test/__mocks__/RequestHelper.ts
@@ -1,14 +1,14 @@
 /* eslint-disable vitest/require-mock-type-parameters */
 import { vi } from 'vitest';
 
-export const RequestHelper = {
-  post: vi.fn(() => vi.fn()),
-  get: vi.fn(() => vi.fn()),
-  put: vi.fn(() => vi.fn()),
-  del: vi.fn(() => vi.fn()),
-  patch: vi.fn(() => vi.fn()),
-};
+const mockHelperFn = vi.fn();
 
 export default {
-  RequestHelper,
+  RequestHelper: {
+    post: () => mockHelperFn,
+    get: () => mockHelperFn,
+    put: () => mockHelperFn,
+    del: () => mockHelperFn,
+    patch: () => mockHelperFn,
+  },
 };
