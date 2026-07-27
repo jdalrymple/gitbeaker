b1dd27abdcfff5c7a348d6c42db6b15102e8fc20
Justin Dalrymple <justin.dalrymple@dialogue.co>
2026-07-27 18:56:55 -0400

    Fixing ref

--- packages/rest/test/integration/browser/General.ts
+++ packages/rest/test/integration/browser/General.ts
@@ -1,5 +1,8 @@
 import { expect, test as it } from '@playwright/test';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const { describe } = it;
 
